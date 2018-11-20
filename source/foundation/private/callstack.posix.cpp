3ef86063b0c9e44302c947934acdc74731ba4825
Sean Middleditch <sean@seanmiddleditch.com>
2018-11-20 02:04:19 -0800

    Fixes for Linux compilation

--- source/foundation/private/callstack.posix.cpp
+++ source/foundation/private/callstack.posix.cpp
@@ -6,15 +6,22 @@
 
 #include <execinfo.h>
 
-int gm::CallStackReader::readCallstack(array_view<uintptr> addresses, int skip, int max)
+int gm::CallStackReader::readCallstack(array_view<uintptr> addresses, int skip)
 {
-    return backtrace(&addresses.data(), std::min(size, addresses.size()));
+    void* buffer;
+
+    int max = std::max(0, addresses.size() - skip);
+    int count = backtrace(&buffer, max);
+    skip = std::min(skip, count);
+
+    std::memcpy(addresses.data(), buffer + skip, std::min(count, max));
 }
 
 bool gm::CallStackReader::tryResolveCallstack(array_view<uintptr const> addresses, array_view<CallStackRecord> out_records)
 {
 #if !defined(NDEBUG)
-    char** symbols = backtrace_symbols(&addresses.data(), addresses.size());
+    void const* addrs = addresses.data();
+    char** symbols = backtrace_symbols(&addrs, addresses.size());
 
     for (auto index = 0; index != addresses.size(); ++index)
     {
