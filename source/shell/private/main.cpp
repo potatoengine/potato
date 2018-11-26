0536eaa3056426b96c2505969e548329f2a58a59
Sean Middleditch <sean@seanmiddleditch.com>
2018-11-25 20:05:52 -0800

    Initial dummy SDL2 window

--- source/shell/private/main.cpp
+++ source/shell/private/main.cpp
@@ -1,7 +1,34 @@
 #include "grimm/foundation/vector.h"
+#include <SDL.h>
 
-int main()
+void Run();
+
+int main(int argc, char* argv[])
 {
     gm::vector<int> v;
+
+    SDL_Window* window = SDL_CreateWindow("Grimm Shell", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_RESIZABLE);
+    Run();
+    SDL_DestroyWindow(window);
+
     return 0;
+}
+
+static void Run()
+{
+    SDL_Event ev;
+    while (SDL_WaitEvent(&ev))
+    {
+        switch (ev.type)
+        {
+        case SDL_QUIT:
+            return;
+        case SDL_WINDOWEVENT:
+            switch (ev.window.type)
+            {
+            case SDL_WINDOWEVENT_CLOSE:
+                return;
+            }
+        }
+    }
 }
