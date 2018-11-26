2adb18a005b37488fa9a4faac8c443c158b2cfe7
Sean Middleditch <sean@seanmiddleditch.com>
2018-11-26 12:49:07 -0800

    Use ubuntu-toolchain-r/test (and ninja)

--- scripts/bootstrap-ubuntu.sh
+++ scripts/bootstrap-ubuntu.sh
@@ -6,12 +6,12 @@ BUILD_DIR="${ROOT}/build/ubuntu"
 
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/ppa
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo add-apt-repository -y ppa:janisozaur/cmake-update
 sudo add-apt-repository -y "deb http://apt.llvm.org/${DISTRIB_CODENAME}/ llvm-toolchain-${DISTRIB_CODENAME}-7 main"
 
 sudo apt-get -y update
-sudo apt-get -y install cmake clang-7 libc++-7-dev libc++abi-7-dev unzip g++-7
+sudo apt-get -y install cmake clang-7 libc++-7-dev libc++abi-7-dev g++-7 ninja-build unzip
 
 export CXX=g++-7
 
@@ -22,7 +22,7 @@ export CXX=clang++-7
 export CXXFLAGS="-m64 -stdlib=libc++"
 
 mkdir -p "$BUILD_DIR/RelWithDebInfo" && cd "$BUILD_DIR/RelWithDebInfo"
-/usr/bin/cmake -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE="$ROOT/vcpkg/scripts/buildsystems/vcpkg.cmake" "$ROOT"
+/usr/bin/cmake -G Ninja -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE="$ROOT/vcpkg/scripts/buildsystems/vcpkg.cmake" "$ROOT"
 
 mkdir -p "$BUILD_DIR/Debug" && cd "$BUILD_DIR/Debug"
-/usr/bin/cmake -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_TOOLCHAIN_FILE="$ROOT/vcpkg/scripts/buildsystems/vcpkg.cmake" "$ROOT"
+/usr/bin/cmake -G Ninja -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_TOOLCHAIN_FILE="$ROOT/vcpkg/scripts/buildsystems/vcpkg.cmake" "$ROOT"
