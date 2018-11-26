715761da7a6bfc4f74553ef66c618c784b043f88
Sean Middleditch <sean@seanmiddleditch.com>
2018-11-26 13:52:41 -0800

    Fix (real) Linux build error

--- source/shell/private/main.cpp
+++ source/shell/private/main.cpp
@@ -1,20 +1,20 @@
 #include "grimm/foundation/vector.h"
 #include <SDL.h>
 
-void Run();
+static void run_shell();
 
 int main(int argc, char* argv[])
 {
     gm::vector<int> v;
 
     SDL_Window* window = SDL_CreateWindow("Grimm Shell", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_RESIZABLE);
-    Run();
+    run_shell();
     SDL_DestroyWindow(window);
 
     return 0;
 }
 
-static void Run()
+static void run_shell()
 {
     SDL_Event ev;
     while (SDL_WaitEvent(&ev))
