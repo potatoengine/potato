d4793254be7994e512ae3a2f40bd555317d32b62
Sean Middleditch <sean@seanmiddleditch.com>
2018-11-30 07:58:28 -0800

    Build fmt with clang on Linux

--- scripts/bootstrap-ubuntu.sh
+++ scripts/bootstrap-ubuntu.sh
@@ -15,12 +15,15 @@ sudo apt-get -y install cmake clang-7 libc++-7-dev libc++abi-7-dev g++-7 ninja-b
 
 export CXX=g++-7
 
+# vcpkg only compiles with g++, and SDL2 is a C library so any compiler works
 bash "$ROOT/vcpkg/bootstrap-vcpkg.sh"
 "$ROOT/vcpkg/vcpkg" install sdl2:x64-linux 
-"$ROOT/vcpkg/vcpkg" install fmt:x64-linux
 
 export CXX=clang++-7
 export CXXFLAGS="-m64 -stdlib=libc++"
+
+# fmt is mostly header-only so it needs to use the same C++ library
+"$ROOT/vcpkg/vcpkg" install fmt:x64-linux
 
 mkdir -p "$BUILD_DIR/RelWithDebInfo" && cd "$BUILD_DIR/RelWithDebInfo"
 /usr/bin/cmake -G Ninja -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE="$ROOT/vcpkg/scripts/buildsystems/vcpkg.cmake" "$ROOT"
