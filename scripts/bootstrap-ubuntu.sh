012911d19784a5840324eae3e05938c1f652e029
Sean Middleditch <sean@seanmiddleditch.com>
2018-12-02 17:16:52 -0800

    Bundle SDL in-tree (#5)
    * Add SDL-2.0.9 to source tree
    
    * Remove vcpkg submodule
    
    * Remove vcpkg config
    
    * Fix shell build
    
    * Remove vcpkg toolchain reference from linux bootstrap

--- scripts/bootstrap-ubuntu.sh
+++ scripts/bootstrap-ubuntu.sh
@@ -11,18 +11,13 @@ sudo add-apt-repository -y ppa:janisozaur/cmake-update
 sudo add-apt-repository -y "deb http://apt.llvm.org/${DISTRIB_CODENAME}/ llvm-toolchain-${DISTRIB_CODENAME}-7 main"
 
 sudo apt-get -y update
-sudo apt-get -y install cmake clang-7 libc++-7-dev libc++abi-7-dev g++-7 ninja-build unzip
-
-export CXX=g++-7
-
-bash "$ROOT/vcpkg/bootstrap-vcpkg.sh"
-"$ROOT/vcpkg/vcpkg" install sdl2:x64-linux
+sudo apt-get -y install cmake clang-7 libc++-7-dev libc++abi-7-dev ninja-build
 
 export CXX=clang++-7
 export CXXFLAGS="-m64 -stdlib=libc++"
 
 mkdir -p "$BUILD_DIR/RelWithDebInfo" && cd "$BUILD_DIR/RelWithDebInfo"
-/usr/bin/cmake -G Ninja -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE="$ROOT/vcpkg/scripts/buildsystems/vcpkg.cmake" "$ROOT"
+/usr/bin/cmake -G Ninja -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo "$ROOT"
 
 mkdir -p "$BUILD_DIR/Debug" && cd "$BUILD_DIR/Debug"
-/usr/bin/cmake -G Ninja -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_TOOLCHAIN_FILE="$ROOT/vcpkg/scripts/buildsystems/vcpkg.cmake" "$ROOT"
+/usr/bin/cmake -G Ninja -DCMAKE_BUILD_TYPE:STRING=Debug "$ROOT"
