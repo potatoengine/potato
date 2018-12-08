23f72bf9a2fe56930814b6c10dc7bc59929c1a4a
Sean Middleditch <sean@seanmiddleditch.com>
2018-12-08 14:48:36 -0800

    Add doctest 2.2.0

---
+++ external/doctest-2.2.0/examples/all_features/alternative_macros.cpp
@@ -0,0 +1,15 @@
+#include "doctest_proxy.h"
+
+my_testcase("custom macros") {
+    my_check(1 == 1);
+    
+    my_check_eq(1, 1);
+    
+    my_subcase("bar") {
+        my_subcase("foo") {}
+        my_subcase("baz") {}
+        
+        my_require(5 > 3);
+    }
+    // CHECK(1 == 1); <== ERROR - the default short macros are disabled from the proxy header
+}
