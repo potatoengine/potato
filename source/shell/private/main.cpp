9a8130e17c77c3b23814ada7cb8891f768415286
Sean Middleditch <sean@seanmiddleditch.com>
2018-12-13 19:19:49 -0800

    Try to compile even without Vulkan or D3D12

--- source/shell/private/main.cpp
+++ source/shell/private/main.cpp
@@ -1,9 +1,13 @@
 #include "grimm/foundation/vector.h"
-#include "grimm/gpu_d3d12/d3d12_factory.h"
 #include "grimm/gpu/device.h"
+
 #include <SDL.h>
 #include <SDL_syswm.h>
 #include <SDL_messagebox.h>
+
+#if GM_USE_D3D12
+#include "grimm/gpu_d3d12/d3d12_factory.h"
+#endif
 
 static void run_shell();
 
@@ -11,6 +15,7 @@ int main(int argc, char* argv[])
 {
     SDL_Window* window = SDL_CreateWindow("Grimm Shell", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_RESIZABLE);
 
+#if GM_USE_D3D12
     SDL_SysWMinfo wmInfo;
     SDL_GetWindowWMInfo(window, &wmInfo);
     HWND hwnd = wmInfo.info.win.window;
@@ -21,6 +26,7 @@ int main(int argc, char* argv[])
         SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Fatal error", "Could not find device", window);
         return 1;
     }
+#endif
 
     run_shell();
     SDL_DestroyWindow(window);
