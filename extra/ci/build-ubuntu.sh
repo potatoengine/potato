1cf3152619602740d48b5a62d1778430b4d862ad
Sean Middleditch <sean.middleditch@gmail.com>
2018-12-19 22:17:55 -0800

    Make our CI scripts more easily able to handle Clang version changes.

--- extra/ci/build-ubuntu.sh
+++ extra/ci/build-ubuntu.sh
@@ -1,9 +1,12 @@
 #/bin/bash
 build_type="${1:-Debug}"
+CLANG_VERSION=7
 
 set -o xtrace
-export CXX=clang++-7
-export CC=clang-7
-export CXXFLAGS="-stdlib=libc++ -m64"
+
+export CXX=clang++-${CLANG_VERSION}
+export CC=clang-${CLANG_VERSION}
+export CXXFLAGS="-m64 -stdlib=libc++"
+
 cmake /source -G Ninja -DCMAKE_BUILD_TYPE:STRING=${build_type}
 cmake --build . --parallel
