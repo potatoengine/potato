65c72e58ac18b8f885a5a042dd50f781b5b7dfdd
Sean Middleditch <sean.middleditch@gmail.com>
2018-12-19 20:00:59 -0800

    Run Linux build in Docker image

--- extra/ci/build-ubuntu.sh
+++ extra/ci/build-ubuntu.sh
@@ -1,7 +1,9 @@
 #/bin/bash
+build_type="${1:-Debug}"
+
 set -o xtrace
 export CXX=clang++-7
 export CC=clang-7
 export CXXFLAGS="-stdlib=libc++ -m64"
-cmake /source
+cmake /source -G Ninja -DCMAKE_BUILD_TYPE:STRING=${build_type}
 cmake --build . --parallel
