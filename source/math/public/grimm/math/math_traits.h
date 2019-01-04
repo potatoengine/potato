dc23a5677c2668cbac38712fde64df013eb245ae
Sean Middleditch <sean.middleditch@gmail.com>
2019-01-03 22:02:36 -0800

    Kill has_shuffle trait

--- source/math/public/grimm/math/math_traits.h
+++ source/math/public/grimm/math/math_traits.h
@@ -13,15 +13,9 @@ namespace gm::_detail {
         template <typename T>
         using test = decltype(T::component_length >= N);
     };
-
-    template <typename T>
-    using _has_shuffle = decltype(static_cast<T*>(0)->shuffle());
 }
 
 namespace gm {
     template <typename T, int MinN>
     constexpr bool is_vector_v = is_detected_v<_detail::_has_value_type, T> && is_detected_v<_detail::_has_size<MinN>::template test, T>;
-
-    template <typename T>
-    constexpr bool has_shuffle_v = is_detected_v<_detail::_has_shuffle, T>;
 } // namespace gm::constants
