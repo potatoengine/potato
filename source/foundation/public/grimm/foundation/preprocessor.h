84515eaec83c3da8bc1b1d5d3167847ab463e541
Sean Middleditch <sean.middleditch@gmail.com>
2019-01-03 23:47:20 -0800

    GM_PP_JOIN and GM_PP_MAP

--- source/foundation/public/grimm/foundation/preprocessor.h
+++ source/foundation/public/grimm/foundation/preprocessor.h
@@ -40,17 +40,31 @@ static_assert(GM_PP_SELECT(0)(2) + 2 == 2);
 static_assert(GM_PP_ARITY(a, b, c, d) == 4);
 static_assert(GM_PP_ARITY(a) == 1);
 
-#define _gm_PP_APPLY0(t, ignore)
-#define _gm_PP_APPLY1(t, a) t(a)
-#define _gm_PP_APPLY2(t, a, b) t(a) t(b)
-#define _gm_PP_APPLY3(t, a, b, c) t(a) t(b) t(c)
-#define _gm_PP_APPLY4(t, a, b, c, d) t(a) t(b) t(c) t(d)
-#define _gm_PP_APPLY5(t, a, b, c, d, e) t(a) t(b) t(c) t(d) t(e)
-#define _gm_PP_APPLY6(t, a, b, c, d, e, f) t(a) t(b) t(c) t(d) t(e) t(f)
+#define _gm_PP_JOIN0(ignore)
+#define _gm_PP_JOIN1(a) a
+#define _gm_PP_JOIN2(a, b) a##b
+#define _gm_PP_JOIN3(a, b, c) a##b##c
+#define _gm_PP_JOIN4(a, b, c, d) a##b##c##d
+#define _gm_PP_JOIN5(a, b, c, d, e) a##b##c##d##e
+#define _gm_PP_JOIN6(a, b, c, d, e, f) a##b##c##d##e##f
 
-#define _gm_PP_APPLY_INVOKE(t, n, ...) GM_PP_EXPAND(_gm_PP_APPLY##n(t, __VA_ARGS__))
-#define _gm_PP_APPLY(t, n, ...) _gm_PP_APPLY_INVOKE(t, n, __VA_ARGS__)
-#define GM_PP_APPLY(t, ...) _gm_PP_APPLY(t, GM_PP_ARITY(__VA_ARGS__), __VA_ARGS__)
+#define _gm_PP_JOIN_INVOKE(n, ...) GM_PP_EXPAND(_gm_PP_JOIN##n(__VA_ARGS__))
+#define _gm_PP_JOIN(n, ...) _gm_PP_JOIN_INVOKE(n, __VA_ARGS__)
+#define GM_PP_JOIN(...) _gm_PP_JOIN(GM_PP_ARITY(__VA_ARGS__), __VA_ARGS__)
 
-#define _gm_PP_APPLY_TEST(x) +x
-static_assert(GM_PP_APPLY(_gm_PP_APPLY_TEST, 1, 2, 3) == 6);
+static_assert(GM_PP_JOIN(1, 2, 3, 4, 5, 6) == 123456);
+
+#define _gm_PP_MAP0(t, ignore)
+#define _gm_PP_MAP1(t, a) t(a)
+#define _gm_PP_MAP2(t, a, b) t(a), t(b)
+#define _gm_PP_MAP3(t, a, b, c) t(a), t(b), t(c)
+#define _gm_PP_MAP4(t, a, b, c, d) t(a), t(b), t(c), t(d)
+#define _gm_PP_MAP5(t, a, b, c, d, e) t(a), t(b), t(c), t(d), t(e)
+#define _gm_PP_MAP6(t, a, b, c, d, e, f) t(a), t(b), t(c), t(d), t(e), t(f)
+
+#define _gm_PP_MAP_INVOKE(t, n, ...) GM_PP_EXPAND(_gm_PP_MAP##n(t, __VA_ARGS__))
+#define _gm_PP_MAP(t, n, ...) _gm_PP_MAP_INVOKE(t, n, __VA_ARGS__)
+#define GM_PP_MAP(t, ...) _gm_PP_MAP(t, GM_PP_ARITY(__VA_ARGS__), __VA_ARGS__)
+
+#define _gm_PP_MAP_TEST(x) +x
+static_assert((0 GM_PP_MAP(_gm_PP_MAP_TEST, 1, 2, 3)) == 3);
