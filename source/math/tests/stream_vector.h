0a5df80e904a2abf0553bb63e1a08ead180b72b6
Sean Middleditch <sean.middleditch@gmail.com>
2019-01-03 22:02:56 -0800

    Fix test output of vector type

--- source/math/tests/stream_vector.h
+++ source/math/tests/stream_vector.h
@@ -1,9 +1,10 @@
-#include <iostream>
 #include "grimm/math/math_traits.h"
+#include <iostream>
+#include <type_traits>
 
 namespace gm {
-    template <typename T, typename = std::enable_if_t<is_vector_v<T>>>
-    std::ostream& operator<<(std::ostream& os, T const& vec) {
+    template <typename T>
+    auto operator<<(std::ostream& os, T const& vec) -> std::enable_if_t<is_vector_v<T>, std::ostream&> {
         os << '{' << vec[0];
         for (int i = 1; i < vec.component_length; ++i) {
             os << ',' << vec[i];
