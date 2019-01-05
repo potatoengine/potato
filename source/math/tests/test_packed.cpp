f9f32feaab1b1321f6395eee8e094f14fd9815e7
Sean Middleditch <sean.middleditch@gmail.com>
2019-01-04 20:18:15 -0800

    Parameterize types for packed::initialization test
    Mostly just verifies that we actually compile with multiple types.

--- source/math/tests/test_packed.cpp
+++ source/math/tests/test_packed.cpp
@@ -3,29 +3,29 @@
 #include "stream_vector.h"
 
 DOCTEST_TEST_SUITE("[grimm][math] PackedVector4f") {
-    DOCTEST_TEST_CASE("initialization") {
+    DOCTEST_TEST_CASE_TEMPLATE("initialization", T, double, float, int) {
         using namespace gm;
 
-        PackedVector4f zero;
+        PackedVector<T, 4> zero;
 
-        DOCTEST_CHECK_EQ(zero.m.x, 0.f);
-        DOCTEST_CHECK_EQ(zero.m.y, 0.f);
-        DOCTEST_CHECK_EQ(zero.m.z, 0.f);
-        DOCTEST_CHECK_EQ(zero.m.w, 0.f);
+        DOCTEST_CHECK_EQ(zero.m.x, 0);
+        DOCTEST_CHECK_EQ(zero.m.y, 0);
+        DOCTEST_CHECK_EQ(zero.m.z, 0);
+        DOCTEST_CHECK_EQ(zero.m.w, 0);
 
-        PackedVector pv{1.f, 2.f, 3.f, 4.f};
+        PackedVector<T, 4> pv{1, 2, 3, 4};
 
-        DOCTEST_CHECK_EQ(pv.m.x, 1.f);
-        DOCTEST_CHECK_EQ(pv.m.y, 2.f);
-        DOCTEST_CHECK_EQ(pv.m.z, 3.f);
-        DOCTEST_CHECK_EQ(pv.m.w, 4.f);
+        DOCTEST_CHECK_EQ(pv.m.x, 1);
+        DOCTEST_CHECK_EQ(pv.m.y, 2);
+        DOCTEST_CHECK_EQ(pv.m.z, 3);
+        DOCTEST_CHECK_EQ(pv.m.w, 4);
 
-        PackedVector4f pv2{PackedVector3f{7.f, -7.f, 0.f}, -1.f};
+        PackedVector<T, 4> pv2{PackedVector<T, 3>{7, -7, 0}, -1};
 
-        DOCTEST_CHECK_EQ(pv2.m.x, 7.f);
-        DOCTEST_CHECK_EQ(pv2.m.y, -7.f);
-        DOCTEST_CHECK_EQ(pv2.m.z, 0.f);
-        DOCTEST_CHECK_EQ(pv2.m.w, -1.f);
+        DOCTEST_CHECK_EQ(pv2.m.x, 7);
+        DOCTEST_CHECK_EQ(pv2.m.y, -7);
+        DOCTEST_CHECK_EQ(pv2.m.z, 0);
+        DOCTEST_CHECK_EQ(pv2.m.w, -1);
     }
 
     DOCTEST_TEST_CASE("equalities") {
