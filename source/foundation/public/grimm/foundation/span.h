26d67fd91b4fcfd222bf5d35387e7605aef14799
Sean Middleditch <sean.middleditch@gmail.com>
2019-01-07 21:55:01 -0800

    constexpr span

--- source/foundation/public/grimm/foundation/span.h
+++ source/foundation/public/grimm/foundation/span.h
@@ -39,42 +39,42 @@ public:
     using size_type = std::size_t;
     using index_type = size_type;
 
-    span() = default;
+    constexpr span() = default;
     template <typename U>
-    /*implicit*/ span(span<U> src) noexcept
+    /*implicit*/ constexpr span(span<U> src) noexcept
         : _begin(src.begin()), _end(src.end()) {}
     template <std::size_t N>
-    /*implicit*/ span(T (&src)[N]) noexcept
+    /*implicit*/ constexpr span(T (&src)[N]) noexcept
         : _begin(src), _end(src + N) {}
-    /*implicit*/ span(T* begin, T* end) noexcept
+    /*implicit*/ constexpr span(T* begin, T* end) noexcept
         : _begin(begin), _end(end) {}
-    /*implicit*/ span(std::initializer_list<T> src) noexcept
+    /*implicit*/ constexpr span(std::initializer_list<T> src) noexcept
         : _begin(src.begin()), _end(src.end()) {}
     template <std::size_t N>
-    /*implicit*/ span(std::array<T, N> src) noexcept : span(src.data(), N) {}
-    /*implicit*/ span(T* ptr, std::size_t size) noexcept
+    /*implicit*/ constexpr span(std::array<T, N> src) noexcept : span(src.data(), N) {}
+    /*implicit*/ constexpr span(T* ptr, std::size_t size) noexcept
         : _begin(ptr), _end(ptr + size) {}
 
-    iterator begin() const noexcept { return _begin; }
-    sentinel end() const noexcept { return _end; }
+    constexpr iterator begin() const noexcept { return _begin; }
+    constexpr sentinel end() const noexcept { return _end; }
 
-    pointer data() const noexcept { return _begin; }
+    constexpr pointer data() const noexcept { return _begin; }
 
-    bool empty() const noexcept { return _begin == _end; }
-    explicit operator bool() const noexcept { return _begin != _end; }
+    constexpr bool empty() const noexcept { return _begin == _end; }
+    constexpr explicit operator bool() const noexcept { return _begin != _end; }
 
-    reference operator[](size_type index) const noexcept { return _begin[index]; }
+    constexpr reference operator[](size_type index) const noexcept { return _begin[index]; }
 
-    size_type size() const noexcept { return static_cast<size_type>(_end - _begin); }
-    size_type size_bytes() const noexcept { return sizeof(T) * static_cast<size_type>(_end - _begin); }
+    constexpr size_type size() const noexcept { return static_cast<size_type>(_end - _begin); }
+    constexpr size_type size_bytes() const noexcept { return sizeof(T) * static_cast<size_type>(_end - _begin); }
 
-    reference front() const noexcept { return *_begin; }
-    reference back() const noexcept { return *(_end - 1); }
+    constexpr reference front() const noexcept { return *_begin; }
+    constexpr reference back() const noexcept { return *(_end - 1); }
 
-    span first(size_type length) const noexcept { return span{_begin, length}; };
-    span last(size_type length) const noexcept { return span{_end - length, length}; };
+    constexpr span first(size_type length) const noexcept { return span{_begin, length}; };
+    constexpr span last(size_type length) const noexcept { return span{_end - length, length}; };
 
-    span subspan(size_type offset, size_type count) const noexcept { return span{_begin + offset, count}; }
+    constexpr span subspan(size_type offset, size_type count) const noexcept { return span{_begin + offset, count}; }
 
     span<std::byte const> as_bytes() const noexcept {
         return {reinterpret_cast<std::byte const*>(_begin), static_cast<size_type>(_end - _begin)};
@@ -83,8 +83,8 @@ public:
         return {reinterpret_cast<std::byte*>(_begin), _end - _begin};
     }
 
-    void pop_front() noexcept { ++_begin; }
-    void pop_back() noexcept { --_end; }
+    constexpr void pop_front() noexcept { ++_begin; }
+    constexpr void pop_back() noexcept { --_end; }
 
 private:
     pointer _begin = nullptr;
