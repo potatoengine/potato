df2e16b28dedab958cd8501730cdc789f2dccdd2
Sean Middleditch <sean.middleditch@gmail.com>
2019-01-12 22:58:26 -0800

    Shared library compilation fixes

---
+++ source/library/private/asset_library.cpp
@@ -0,0 +1,6 @@
+// Copyright (C) 2019 Sean Middleditch, all rights reserverd.
+
+#include "_export.h"
+#include "grimm/library/asset_library.h"
+
+gm::AssetLibrary::~AssetLibrary() = default;
---
+++ source/library/public/grimm/library/asset_library.h
@@ -0,0 +1,14 @@
+// Copyright (C) 2019 Sean Middleditch, all rights reserverd.
+
+#pragma once
+
+#include "_export.h"
+#include "grimm/foundation/vector.h"
+
+namespace gm {
+    class AssetLibrary {
+    public:
+        AssetLibrary() = default;
+        GM_LIBRARY_API ~AssetLibrary();
+    };
+} // namespace gm
