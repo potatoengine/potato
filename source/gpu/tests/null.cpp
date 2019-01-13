ba9f48805a01edf6668b3d8cbb22e1d4df0ac634
Sean Middleditch <sean.middleditch@gmail.com>
2019-01-12 19:32:24 -0800

    Shared library compilation fixes

--- source/gpu/tests/null.cpp
+++ source/gpu/tests/null.cpp
@@ -6,12 +6,12 @@ DOCTEST_TEST_SUITE("[grimm][gpu] NullDevice") {
     DOCTEST_TEST_CASE("factory enumerates") {
         using namespace gm;
 
-        NullFactory factory;
+        auto factory = CreateNullGPUFactory();
 
-        DOCTEST_CHECK(factory.isEnabled());
+        DOCTEST_CHECK(factory->isEnabled());
 
         bool enumerated = false;
-        factory.enumerateDevices([&](auto const& deviceInfo) {
+        factory->enumerateDevices([&](auto const& deviceInfo) {
             DOCTEST_CHECK_EQ(deviceInfo.index, 0);
 
             // ensure we only get one
@@ -23,24 +23,25 @@ DOCTEST_TEST_SUITE("[grimm][gpu] NullDevice") {
     DOCTEST_TEST_CASE("factory abides") {
         using namespace gm;
 
-        NullFactory factory;
+        auto factory = CreateNullGPUFactory();
 
-        auto device = factory.createDevice(0);
+        auto device = factory->createDevice(0);
         DOCTEST_CHECK_NE(device, nullptr);
     }
 
     DOCTEST_TEST_CASE("device abides") {
         using namespace gm;
 
-        NullDevice device;
+        auto factory = CreateNullGPUFactory();
+        auto device = factory->createDevice(0);
 
-        auto swapChain = device.createSwapChain(nullptr);
+        auto swapChain = device->createSwapChain(nullptr);
         DOCTEST_CHECK_NE(swapChain, nullptr);
 
-        auto descriptorHeap = device.createDescriptorHeap();
+        auto descriptorHeap = device->createDescriptorHeap();
         DOCTEST_CHECK_NE(descriptorHeap, nullptr);
 
-        auto pipelineState = device.createPipelineState();
+        auto pipelineState = device->createPipelineState();
         DOCTEST_CHECK_NE(pipelineState, nullptr);
     }
 }
