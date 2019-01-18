3cf0205f4609eb9c9ef80e18ad09b67ba4709783
Sean Middleditch <sean.middleditch@gmail.com>
2019-01-17 17:49:27 -0800

    Add tests for the (read-only) native file system operations
    Write tests will need some kind of support for ensuring/restoring filesystem contents before a test run.

---
+++ source/filesystem/tests/test_native_backend.cpp
@@ -0,0 +1,67 @@
+#include "grimm/foundation/vector.h"
+#include "grimm/filesystem/native_backend.h"
+#include "doctest.h"
+#include <string>
+#include <algorithm>
+
+DOCTEST_TEST_SUITE("[grimm][filesystem] gm::fs::NativeBackend") {
+    using namespace gm;
+    using namespace gm::fs;
+
+    DOCTEST_TEST_CASE("fileExists") {
+        auto native = NativeBackend::create();
+
+        DOCTEST_CHECK(native.fileExists("test.txt"));
+        DOCTEST_CHECK(!native.fileExists("foobar.txt"));
+
+        DOCTEST_CHECK(!native.fileExists("parent"));
+    }
+
+    DOCTEST_TEST_CASE("directoryExists") {
+        auto native = NativeBackend::create();
+
+        DOCTEST_CHECK(native.directoryExists("parent"));
+        DOCTEST_CHECK(native.directoryExists("parent/child"));
+
+        DOCTEST_CHECK(!native.directoryExists("parent/child/grandchild"));
+        DOCTEST_CHECK(!native.directoryExists("test.txt"));
+    }
+
+    DOCTEST_TEST_CASE("openRead") {
+        auto native = NativeBackend::create();
+
+        auto inFile = native.openRead("test.txt");
+        DOCTEST_CHECK(inFile.is_open());
+
+        std::string text;
+        std::getline(inFile, text);
+
+        DOCTEST_CHECK_EQ(text, "This is a test.");
+    }
+
+    DOCTEST_TEST_CASE("enumerate") {
+        vector<std::string> const expected = {
+            "parent",
+            "parent/child",
+            "parent/child/hello.txt",
+            "test.txt"};
+
+        auto native = NativeBackend::create();
+
+        vector<std::string> entries;
+
+        auto cb(EnumerateCallback{[&entries](FileInfo const& info) {
+            entries.push_back(info.path);
+            return EnumerateResult::Recurse;
+        }});
+        DOCTEST_CHECK_EQ(native.enumerate(".", cb), Result::Success);
+
+        std::sort(entries.begin(), entries.end());
+
+        DOCTEST_CHECK(std::equal(expected.begin(), expected.end(), entries.begin(), entries.end()));
+    }
+
+    // openWrite
+    // null.createDirectories("/foo/bar"), Result::UnsupportedOperation);
+    // null.copyFile("/test.txt", "/out.txt"), Result::UnsupportedOperation);
+}
