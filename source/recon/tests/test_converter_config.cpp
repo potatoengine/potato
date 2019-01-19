e94a587b4feef012669258509f3ecdd0706eb5e8
Sean Middleditch <sean.middleditch@gmail.com>
2019-01-18 23:29:02 -0800

    Attempt to work around the Clang linker error

--- source/recon/tests/test_converter_config.cpp
+++ source/recon/tests/test_converter_config.cpp
@@ -13,9 +13,9 @@ DOCTEST_TEST_SUITE("[grimm][recon] ConverterConfig") {
         bool ok = parseArguments(config, args);
         DOCTEST_CHECK(ok);
 
-        DOCTEST_CHECK_EQ(config.sourceFolderPath, "ABC");
-        DOCTEST_CHECK_EQ(config.destinationFolderPath, "DEF");
-        DOCTEST_CHECK_EQ(config.cacheFolderPath, "GHI");
+        DOCTEST_CHECK_EQ(config.sourceFolderPath.c_str(), "ABC");
+        DOCTEST_CHECK_EQ(config.destinationFolderPath.c_str(), "DEF");
+        DOCTEST_CHECK_EQ(config.cacheFolderPath.c_str(), "GHI");
     }
 
     DOCTEST_TEST_CASE("json") {
@@ -25,8 +25,8 @@ DOCTEST_TEST_SUITE("[grimm][recon] ConverterConfig") {
         bool ok = parseConfigString(config, json);
         DOCTEST_CHECK(ok);
 
-        DOCTEST_CHECK_EQ(config.sourceFolderPath, "ABC");
-        DOCTEST_CHECK_EQ(config.destinationFolderPath, "DEF");
-        DOCTEST_CHECK_EQ(config.cacheFolderPath, "GHI");
+        DOCTEST_CHECK_EQ(config.sourceFolderPath.c_str(), "ABC");
+        DOCTEST_CHECK_EQ(config.destinationFolderPath.c_str(), "DEF");
+        DOCTEST_CHECK_EQ(config.cacheFolderPath.c_str(), "GHI");
     }
 }
