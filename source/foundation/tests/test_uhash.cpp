c9df640dfc3a9655467de3e517aadf3bdc764815
Sean Middleditch <sean.middleditch@gmail.com>
2019-01-20 09:43:45 -0800

    default_hash fix

--- source/foundation/tests/test_uhash.cpp
+++ source/foundation/tests/test_uhash.cpp
@@ -1,5 +1,6 @@
 #include "grimm/foundation/uhash.h"
 #include "grimm/foundation/string_view.h"
+#include "grimm/foundation/zstring_view.h"
 #include "grimm/foundation/span.h"
 #include "doctest.h"
 
@@ -9,6 +10,7 @@ DOCTEST_TEST_SUITE("[grimm][foundation] gm::uhash") {
     DOCTEST_TEST_CASE("default hash_value") {
         DOCTEST_CHECK_EQ(hash_value<fnv1a>('x'), 0xaf63f54c86021707);
         DOCTEST_CHECK_EQ(hash_value<fnv1a>(string_view("hello world")), 0x779a65e7023cd2e7);
-        DOCTEST_CHECK_EQ(hash_value<fnv1a>(span{1, 2, 3, 4, 5}), 0xd80d6eaea7dc8252);
+        DOCTEST_CHECK_EQ(hash_value<fnv1a>(zstring_view("hello world")), 0x779a65e7023cd2e7);
+        DOCTEST_CHECK_EQ(hash_value<fnv1a>(span{{1, 2, 3, 4, 5}}), 0xd80d6eaea7dc8252);
     }
 }
