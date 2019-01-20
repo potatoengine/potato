dfd79ad9be39a7b61ff4bb64c8bdc121158facfa
Sean Middleditch <sean.middleditch@gmail.com>
2019-01-20 15:52:23 -0800

    More stream features

--- source/filesystem/public/grimm/filesystem/stream.h
+++ source/filesystem/public/grimm/filesystem/stream.h
@@ -7,44 +7,53 @@
 #include "common.h"
 
 namespace gm::fs {
-    class StreamBackend : public shared<StreamBackend> {
+    class Stream {
     public:
         using size_type = uint64;
         using difference_type = int64;
 
-        virtual ~StreamBackend() = default;
+        class Backend : public shared<Backend> {
+        public:
+            virtual ~Backend() = default;
 
-        virtual bool isOpen() const noexcept = 0;
-        virtual bool canRead() const noexcept = 0;
-        virtual bool canWrite() const noexcept = 0;
-        virtual bool canSeek() const noexcept = 0;
+            virtual bool isOpen() const noexcept = 0;
+            virtual bool isEof() const noexcept = 0;
+            virtual bool canRead() const noexcept = 0;
+            virtual bool canWrite() const noexcept = 0;
+            virtual bool canSeek() const noexcept = 0;
 
-        virtual Result seek(SeekPosition position, difference_type offset) = 0;
+            virtual Result seek(SeekPosition position, difference_type offset) = 0;
+            virtual difference_type tell() const = 0;
+            virtual difference_type remaining() const = 0;
 
-        virtual Result read(span<byte>& buffer) = 0;
-        virtual Result write(span<byte const>& buffer) = 0;
-    };
+            virtual Result read(span<byte>& buffer) = 0;
+            virtual Result write(span<byte const> buffer) = 0;
+            virtual Result flush() = 0;
+        };
 
-    class Stream {
-    public:
-        using size_type = StreamBackend::size_type;
-        using difference_type = StreamBackend::difference_type;
-
-        Stream(rc<StreamBackend> impl) : _impl(std::move(impl)) {}
+        Stream() = default;
+        Stream(rc<Backend> impl) noexcept : _impl(std::move(impl)) {}
+        Stream(std::nullptr_t) noexcept {}
 
         bool isOpen() const noexcept { return _impl != nullptr && _impl->isOpen(); }
+        bool isEof() const noexcept { return _impl == nullptr || _impl->isEof(); }
         bool canRead() const noexcept { return _impl != nullptr && _impl->canRead(); }
         bool canWrite() const noexcept { return _impl != nullptr && _impl->canWrite(); }
         bool canSeek() const noexcept { return _impl != nullptr && _impl->canSeek(); }
 
+        explicit operator bool() const noexcept { return isOpen(); }
+
         Result seek(SeekPosition position, difference_type offset) { return _impl->seek(position, offset); }
+        difference_type tell() const { return _impl->tell(); }
+        difference_type remaining() const { return _impl->remaining(); }
 
         Result read(span<byte>& buffer) { return _impl->read(buffer); }
-        Result write(span<byte const>& buffer) { return _impl->write(buffer); }
+        Result write(span<byte const> buffer) { return _impl->write(buffer); }
+        Result flush() { return _impl->flush(); }
 
         void close() { _impl.reset(); }
 
     private:
-        rc<StreamBackend> _impl;
+        rc<Backend> _impl;
     };
 } // namespace gm::fs
