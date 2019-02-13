b6b226fdf123050024efd19dedb78aaefbbc3d59
Sean Middleditch <sean.middleditch@gmail.com>
2019-02-12 22:37:06 -0800

    gm_copy_library_import support for RELEASE/DEBUG separate libraries

--- cmake/modules/gm_copy_library_import.cmake
+++ cmake/modules/gm_copy_library_import.cmake
@@ -9,17 +9,24 @@ function(gm_copy_library_import LIBRARY_TARGET DESTINATION_TARGET)
 
         get_target_property(LIBRARY_PATH "${LIBRARY_TARGET}" IMPORTED_LOCATION)
 
-        if("${LIBRARY_PATH}" STREQUAL "")
-            message(FATAL_ERROR "${LIBRARY_TARGET} has no IMPORTED library to copy")
-        endif()
+        if(NOT LIBRARY_PATH)
+            get_target_property(LIBRARY_PATH_RELEASE "${LIBRARY_TARGET}" IMPORTED_LOCATION_RELEASE)
+            get_target_property(LIBRARY_PATH_DEBUG "${LIBRARY_TARGET}" IMPORTED_LOCATION_DEBUG)
+            if(NOT LIBRARY_PATH_RELEASE AND NOT LIBRARY_PATH_DEBUG)
+                message(FATAL_ERROR "No IMPORTED_LOCATION specified for SHARED import target ${LIBRARY_TARGET}")
+            endif()
 
+            # $<$<CONFIG:DEBUG>:${LIBRARY_PATH_DEBUG}>
+            # $<$<NOT:$<CONFIG:DEBUG>>:${LIBRARY_PATH_RELEASE}>
+            set(LIBRARY_PATH "${LIBRARY_PATH_RELEASE}")
+        endif(NOT LIBRARY_PATH)
+        
         get_filename_component(LIBRARY_FILENAME "${LIBRARY_PATH}" NAME)
-
         add_custom_command(TARGET "${DESTINATION_TARGET}" POST_BUILD
             DEPENDS shell
             MAIN_DEPENDENCY "${LIBRARY_PATH}"
-            COMMENT "Copying ${LIBRARY_FILENAME} to ${DESTINATION_TARGET} output folder"
-            COMMAND "${CMAKE_COMMAND}" -E copy "${LIBRARY_PATH}" "$<TARGET_FILE_DIR:${DESTINATION_TARGET}>/${LIBRARY_FILENAME}"
+            COMMENT "Copying $<TARGET_FILE:${LIBRARY_PATH}> to ${DESTINATION_TARGET} output folder"
+            COMMAND "${CMAKE_COMMAND}" -E copy_if_different "$<TARGET_FILE:${LIBRARY_TARGET}>" "$<TARGET_FILE_DIR:${DESTINATION_TARGET}>/$<TARGET_FILE_NAME:${LIBRARY_TARGET}>"
         )
     endif(WIN32)
 endfunction(gm_copy_library_import)
