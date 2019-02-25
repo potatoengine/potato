41dce6dc43c5bb1993d363761ebb91bf6add7af4
Sean Middleditch <sean@seanmiddleditch.com>
2019-02-24 21:52:36 -0800

    Formatxx (#48)
    * Add formatxx sub-project
    
    * Include formatxx in build
    
    * Use formatxx in foundation
    
    * Remove fmtlib
    
    * Change formatxx url because Azure can't read the git URL

--- source/foundation/public/grimm/foundation/string_format.h
+++ source/foundation/public/grimm/foundation/string_format.h
@@ -4,7 +4,6 @@
 
 #include "debug.h"
 #include "string_view.h"
-#include <iterator>
 
 // needed by fmtlib
 #if !defined(FMT_ASSERT)
@@ -21,11 +20,27 @@
         FMT_ASSERT((cond), "assertion failed")
 #endif
 
-#include <fmt/format.h>
+#include <formatxx/format.h>
 
 namespace gm {
+    template <typename Buffer>
+    class format_writer : public formatxx::format_writer {
+    public:
+        constexpr format_writer(Buffer& buffer) noexcept : _buffer(buffer) {}
+
+        void write(formatxx::string_view str) noexcept override {
+            for (char ch : str) {
+                _buffer.push_back(ch);
+            }
+        }
+
+    private:
+        Buffer& _buffer;
+    };
+
     template <typename Buffer, typename... Args>
     constexpr decltype(auto) format_into(Buffer& buffer, string_view format, Args const&... args) {
-        return ::fmt::format_to(std::back_inserter(buffer), ::fmt::string_view(format.data(), format.size()), args...);
+        format_writer<Buffer> writer(buffer);
+        return ::formatxx::format(writer, ::formatxx::string_view(format.data(), format.size()), args...);
     }
 } // namespace gm
