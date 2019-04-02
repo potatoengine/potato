8e78f57585f36afd536d6456fadfc061282a933a
Sean Middleditch <sean.middleditch@gmail.com>
2019-04-01 22:15:21 -0700

    Non-Windows fixes

--- source/library/concurrency/private/thread_util.darwin.cpp
+++ source/library/concurrency/private/thread_util.darwin.cpp
@@ -1,6 +1,6 @@
 // Copyright (C) 2019 Sean Middleditch, all rights reserverd.
 
-#include "thread_util.h"
+#include "grimm/concurrency/thread_util.h"
 #include <pthread.h>
 
 void gm::concurrency::setCurrentThreadName(zstring_view name) {
