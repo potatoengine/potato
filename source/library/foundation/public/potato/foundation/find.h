e05ec4e2eb4fb16527c08f573a1fe9479073705f
Sean Middleditch <sean.middleditch@gmail.com>
2019-04-14 11:33:51 -0700

    find algorithm implementations
    find (with projection), find_if, any, and all

---
+++ source/library/foundation/public/potato/foundation/find.h
@@ -0,0 +1,90 @@
+// Copyright (C) 2019 Sean Middleditch, all rights reserverd.
+
+#pragma once
+
+namespace up {
+    template <class C>
+    constexpr auto begin(C& c) noexcept(noexcept(c.begin())) { return c.begin(); }
+    template <class C>
+    constexpr auto begin(const C& c) noexcept(noexcept(c.begin())) { return c.begin(); }
+    template <class T, size_t N>
+    constexpr T* begin(T (&array)[N]) noexcept { return array; }
+
+    template <class C>
+    constexpr auto end(C& c) noexcept(noexcept(c.end())) { return c.end(); }
+    template <class C>
+    constexpr auto end(const C& c) noexcept(noexcept(c.end())) { return c.end(); }
+    template <class T, size_t N>
+    constexpr T* end(T (&array)[N]) noexcept { return array + N; }
+
+    struct identity {
+        template <typename T>
+        constexpr T const& operator()(T const& value) const noexcept {
+            return value;
+        }
+    };
+
+    struct equality {
+        template <typename T, typename U>
+        constexpr bool operator()(T const& lhs, U const& rhs) const noexcept(noexcept(lhs == rhs)) {
+            return lhs == rhs;
+        }
+    };
+
+    template <typename C, typename T, typename E = equality, typename P = identity>
+    constexpr auto find(C const& container, T const& value, E const& equals = {}, P const& projection = {}) noexcept(noexcept(projection(*begin(container)))) {
+        auto iter = begin(container);
+        auto last = end(container);
+
+        for (; iter != last; ++iter) {
+            if (equals(projection(*iter), value)) {
+                return iter;
+            }
+        }
+
+        return last;
+    }
+
+    template <typename C, typename P>
+    constexpr auto find_if(C const& container, P const& predicate) noexcept(noexcept(predicate(*begin(container)))) {
+        auto iter = begin(container);
+        auto last = end(container);
+
+        for (; iter != last; ++iter) {
+            if (predicate(*iter)) {
+                return iter;
+            }
+        }
+
+        return last;
+    }
+
+    template <typename C, typename P>
+    constexpr auto any(C const& container, P const& predicate) noexcept(noexcept(predicate(*begin(container)))) {
+        auto iter = begin(container);
+        auto last = end(container);
+
+        for (; iter != last; ++iter) {
+            if (predicate(*iter)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    template <typename C, typename P>
+    constexpr auto all(C const& container, P const& predicate) noexcept(noexcept(predicate(*begin(container)))) {
+        auto iter = begin(container);
+        auto last = end(container);
+
+        for (; iter != last; ++iter) {
+            if (!predicate(*iter)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+} // namespace up
---
+++ source/library/foundation/tests/test_find.cpp
@@ -0,0 +1,48 @@
+#include "potato/foundation/find.h"
+#include "potato/foundation/vector.h"
+#include <doctest/doctest.h>
+#include <iostream>
+
+namespace {
+    constexpr bool is_even(int val) noexcept { return val % 2 == 0; }
+    constexpr bool is_negative(int val) noexcept { return val < 0; }
+}
+
+DOCTEST_TEST_SUITE("[potato][foundation] find algorithms") {
+    using namespace up;
+
+    int array[] = {4, 7, 9, 2, 1, 0, 900};
+    vector vec{-88, 4, 0, 7};
+
+    DOCTEST_TEST_CASE("find") {
+        DOCTEST_CHECK_EQ(find(array, 4), std::begin(array));
+        DOCTEST_CHECK_EQ(find(vec, -88), std::begin(vec));
+
+        DOCTEST_CHECK_EQ(find(array, 900), std::end(array) - 1);
+        DOCTEST_CHECK_EQ(find(vec, 7), std::end(vec) - 1);
+
+        DOCTEST_CHECK_EQ(find(array, 12948734), std::end(array));
+        DOCTEST_CHECK_EQ(find(vec, 12948734), std::end(vec));
+    }
+
+    DOCTEST_TEST_CASE("find_if") {
+        DOCTEST_CHECK_EQ(find_if(array, is_even), std::begin(array));
+        DOCTEST_CHECK_EQ(find_if(vec, is_negative), std::begin(vec));
+
+        DOCTEST_CHECK_EQ(find_if(array, [](int i){ return i > 100; }), std::end(array) - 1);
+        DOCTEST_CHECK_EQ(find_if(vec, [](int i){ return !is_even(i); }), std::end(vec) - 1);
+
+        DOCTEST_CHECK_EQ(find_if(array, [](int){ return false; }), std::end(array));
+        DOCTEST_CHECK_EQ(find_if(vec, [](int){ return false; }), std::end(vec));
+    }
+
+    DOCTEST_TEST_CASE("any") {
+        DOCTEST_CHECK_EQ(any(array, is_negative), false);
+        DOCTEST_CHECK_EQ(any(vec, is_negative), true);
+    }
+
+    DOCTEST_TEST_CASE("all") {
+        DOCTEST_CHECK_EQ(all(array, [](int i) { return !is_negative(i); }), true);
+        DOCTEST_CHECK_EQ(all(vec, is_negative), false);
+    }
+}
