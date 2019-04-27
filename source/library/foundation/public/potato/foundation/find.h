0107e30cdb25da94c86bde544fa1899c10d89c29
Sean Middleditch <sean.middleditch@gmail.com>
2019-04-27 11:47:35 -0700

    Split general utility functions from find.h to utility.h

--- source/library/foundation/public/potato/foundation/find.h
+++ source/library/foundation/public/potato/foundation/find.h
@@ -2,47 +2,10 @@
 
 #pragma once
 
-#include "traits.h"
+#include "utility.h"
 #include <cstdint>
 
 namespace up {
-    template <class C>
-    constexpr auto begin(C& c) noexcept(noexcept(c.begin())) { return c.begin(); }
-    template <class C>
-    constexpr auto begin(const C& c) noexcept(noexcept(c.begin())) { return c.begin(); }
-    template <class T, std::size_t N>
-    constexpr T* begin(T (&array)[N]) noexcept { return array; }
-
-    template <class C>
-    constexpr auto end(C& c) noexcept(noexcept(c.end())) { return c.end(); }
-    template <class C>
-    constexpr auto end(const C& c) noexcept(noexcept(c.end())) { return c.end(); }
-    template <class T, std::size_t N>
-    constexpr T* end(T (&array)[N]) noexcept { return array + N; }
-
-    struct identity {
-        template <typename T>
-        constexpr T const& operator()(T const& value) const noexcept {
-            return value;
-        }
-    };
-
-    struct equality {
-        template <typename T, typename U>
-        constexpr bool operator()(T const& lhs, U const& rhs) const noexcept(noexcept(lhs == rhs)) {
-            return lhs == rhs;
-        }
-    };
-
-    template <class T, class P>
-    decltype(auto) project(P const& projection, T const& value) noexcept(noexcept(invoke(projection, value))) {
-        return invoke(projection, value);
-    }
-    template <class T, class M>
-    M project(M T::*member, T const& value) noexcept {
-        return value.*member;
-    }
-
     template <typename C, typename T, typename E = equality, typename P = identity>
     constexpr auto find(C const& container, T const& value, E const& equals = {}, P const& projection = {}) noexcept(noexcept(project(projection, *begin(container)))) {
         auto iter = begin(container);
