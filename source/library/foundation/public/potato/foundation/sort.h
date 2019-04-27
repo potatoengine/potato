f8c992f9373d8a81294dd7d8a6dd066aa0e480f6
Sean Middleditch <sean.middleditch@gmail.com>
2019-04-27 11:47:53 -0700

    Add a sort.h with a range-based sort with projection feature

---
+++ source/library/foundation/public/potato/foundation/sort.h
@@ -0,0 +1,20 @@
+// Copyright (C) 2019 Sean Middleditch, all rights reserverd.
+
+#pragma once
+
+#include "utility.h"
+#include <algorithm>
+
+namespace up {
+    template <typename Range, typename Compare = std::less<>, typename Projection = identity>
+    constexpr void sort(Range& range, Compare const& compare = {}, Projection const& projection = {}) noexcept(noexcept(project(projection, *begin(range)))) {
+        auto first = begin(range);
+        auto last = end(range);
+
+        auto comparer = [&](auto const& lhs, auto const& rhs) {
+            return compare(project(projection, lhs), project(projection, rhs));
+        };
+
+        std::sort(first, last, comparer);
+    }
+} // namespace up
