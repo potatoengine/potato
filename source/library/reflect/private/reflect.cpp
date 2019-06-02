2f4d10f408f56e6548957db961f66dbf7a87108f
Sean Middleditch <sean.middleditch@gmail.com>
2019-06-01 22:44:25 -0700

    Stub of reflect library

---
+++ source/library/reflect/private/reflect.cpp
@@ -0,0 +1,7 @@
+// Copyright (C) 2019 Potato Engine authors and contributors, all rights reserverd.
+
+#include "potato/reflect/reflect.h"
+
+auto up::example(int x) noexcept -> int {
+    return x;
+}
---
+++ source/library/reflect/public/potato/reflect/reflect.h
@@ -0,0 +1,9 @@
+// Copyright (C) 2019 Potato Engine authors and contributors, all rights reserverd.
+
+#include "_export.h"
+
+namespace up {
+
+    UP_REFLECT_API auto example(int x) noexcept -> int;
+
+} // namespace up
