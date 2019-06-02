3dcc2d29bae8befeccd95d7a29d7d52de2f5a648
Sean Middleditch <sean.middleditch@gmail.com>
2019-06-01 19:12:12 -0700

    Template files for create library script

---
+++ scripts/vs-template/files/private/__name__.cpp
@@ -0,0 +1,3 @@
+// Copyright (C) @YEAR@ Potato Engine authors and contributors, all rights reserverd.
+
+#include "potato/@NAME@/@NAME@.h"
---
+++ scripts/vs-template/files/public/potato/__name__/__name__.h
@@ -0,0 +1,7 @@
+// Copyright (C) @YEAR@ Potato Engine authors and contributors, all rights reserverd.
+
+#include "_export.h"
+
+namespace up {
+
+} // namespace up
