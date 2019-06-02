d5da7ad9d91776f45a24e43698a3edb4723461a7
Sean Middleditch <sean.middleditch@gmail.com>
2019-06-01 21:09:53 -0700

    Add the new library to the CMakeLists.txt of target directory automatically

--- scripts/create_library/create.py
+++ scripts/create_library/create.py
@@ -25,9 +25,9 @@ def main(argv):
         dest_dir = args.dest
     else:
         dest_dir = os.path.relpath(os.path.join(source_dir, '..', '..', '..', 'source', 'library'))
-    dest_dir = os.path.realpath(os.path.join(dest_dir, name))
+    target_dir = os.path.realpath(os.path.join(dest_dir, name))
 
-    print('Creating `{1}` from `{2}`'.format(name, dest_dir, source_dir))
+    print('Creating `{1}` from `{2}`'.format(name, target_dir, source_dir))
 
     for source_file in glob.iglob(pathname=os.path.join(source_dir, '**', '*'), recursive=True):
         if os.path.isdir(source_file):
@@ -36,7 +36,7 @@ def main(argv):
         relative_path=os.path.relpath(source_file, source_dir)
         dir_name=os.path.dirname(relative_path)
 
-        target_dir_name=os.path.join(dest_dir, dir_name).replace('__name__', name)
+        target_dir_name=os.path.join(target_dir, dir_name).replace('__name__', name)
 
         root_file, ext = os.path.splitext(relative_path)
 
@@ -44,7 +44,7 @@ def main(argv):
             os.makedirs(target_dir_name)
 
         if ext == '.in':
-            target_file=os.path.join(dest_dir, root_file).replace('__name__', name)
+            target_file=os.path.join(target_dir, root_file).replace('__name__', name)
             with open(source_file, 'rt') as template_file:
                 template = template_file.read()
 
@@ -54,9 +54,14 @@ def main(argv):
             with open(target_file, 'wt') as output_file:
                output_file.write(processed)
         else:
-            target_file=os.path.join(dest_dir, relative_path).replace('__name__', name)
+            target_file=os.path.join(target_dir, relative_path).replace('__name__', name)
             print('copy to', target_file)
             shutil.copyfile(src=source_file, dst=target_file)
+
+    cmakelist_path = os.path.join(dest_dir, 'CMakeLists.txt')
+    if os.path.isfile(cmakelist_path):
+        with open(cmakelist_path, 'at') as cmake_file:
+            cmake_file.write('add_subdirectory({0})\n'.format(name))
 
 if __name__ == '__main__':
     main(sys.argv)
