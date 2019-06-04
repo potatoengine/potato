b12bd4ad8bb059df195a5d94b26f8fb87b3623fe
Marcin Wieczorek <marcinwie@gmail.com>
2019-06-04 09:42:28 -0700

    moving uuid storage to a const std::array<> container - removing need for platform includes in a header
    fixing linking issues on linux OS
    adding string compare tests to test_uuid

--- source/library/foundation/private/uuid.cpp
+++ source/library/foundation/private/uuid.cpp
@@ -5,49 +5,67 @@
 
 using namespace up;
 
-
+#ifdef UP_PLATFORM_WINDOWS
+#    include "potato/foundation/platform_windows.h"
+#    include <rpc.h>
+#elif defined UP_PLATFORM_LINUX
+#    include <uuid/uuid.h>
+#else
+#    error "Unsupported platform"
+#endif
 
 uuid::uuid() noexcept {
-    std::memset(&_value, 0, sizeof(_value));
+    std::memset(&_data, 0, sizeof(_data));
 }
 
 uuid::uuid(const uuid& other) noexcept {
     if (this == &other)
         return;
-    std::memcpy(&_value, &other._value, sizeof(_value));
+    std::memcpy(&_data, &other._data, sizeof(_data));
 }
 
-uuid::uuid(const uuid_t& other) noexcept {
-    std::memcpy(&_value, &other, sizeof(_value));
+uuid::uuid(const buffer& value) noexcept {
+    std::memcpy(_data.data(), value.data(), sizeof(_data));
 }
 
 auto uuid::isValid() noexcept -> bool {
     return *this != uuid::zero();
 }
+auto generateGuid() -> uuid::buffer {
 
-uuid uuid::generate() {
-
-#ifdef WIN32
+    uuid::buffer ret;
+#ifdef UP_PLATFORM_WINDOWS
     UUID temp;
     if (RPC_S_OK != UuidCreate(&temp))
         UP_ASSERT(false, "Failed to generate unique ID.");
-#else
-    uuid_t temp;
-    uuid_generate_random(temp);
+
+    static_assert(sizeof(ret) == sizeof(UUID));
+    std::memcpy(ret.data(), &temp, sizeof(UUID));
+#elif UP_PLATFORM_LINUX
+    static_assert(sizeof(ret) == sizeof(uuid));
+    uuid_generate((unsigned char*)ret.data());
 #endif
 
+    return ret;
+}
+
+uuid uuid::generate() {
+
+    uuid::buffer temp = generateGuid();
     return uuid(temp);
 }
 
 uuid uuid::zero() {
-    return uuid({0});
+    static uuid _zero({});
+    return _zero;
 }
 
 string uuid::toString(const uuid& id) {
 
-#ifdef WIN32
+#ifdef UP_PLATFORM_WINDOWS
     RPC_CSTR str;
-    if (UuidToStringA(&id._value, &str) != RPC_S_OK)
+    const UUID* data = (const UUID*)id._data.data();
+    if (UuidToStringA(data, &str) != RPC_S_OK)
         return "";
 
     string s((const char*)str);
@@ -55,9 +73,8 @@ string uuid::toString(const uuid& id) {
     return s;
 
 #else
-    uuid_t uuid;
-    uuid_generate_random(uuid);
     char s[37];
-    uuid_unparse(uuid, s);
+    uuid_unparse((unsigned char*)id._data.data(), s);
+    return s;
 #endif
 }
--- source/library/foundation/public/potato/foundation/uuid.h
+++ source/library/foundation/public/potato/foundation/uuid.h
@@ -4,26 +4,21 @@
 
 #include "_export.h"
 #include "potato/foundation/string.h"
-
-#ifdef UP_PLATFORM_WINDOWS
-#    include <windows.h>
-#elif defined UP_PLATFORM_LINUX
-#    include <uuid/uuid.h>
-#else
-#    error "Unsupported platform"
-#endif
+#include <array>
 
 namespace up {
 
     class UP_FOUNDATION_API uuid {
     public:
+        using buffer = std::array<up::byte, 16>;
+
         uuid() noexcept;
         uuid(const uuid& other) noexcept;
-        uuid(const uuid_t& other) noexcept;
+        uuid(const buffer& value) noexcept;
 
         bool isValid() noexcept;
 
-        bool operator==(const uuid& other) const { return std::memcmp(&_value, &other._value, sizeof(_value)) == 0; }
+        bool operator==(const uuid& other) const { return std::memcmp(&_data, &other._data, sizeof(_data)) == 0; }
         bool operator!=(const uuid& other) const { return !operator==(other); }
 
         static uuid generate();
@@ -31,7 +26,7 @@ namespace up {
 
         static uuid zero();
 
-        uuid_t _value;
+        buffer _data;
     };
 
 } // namespace up
