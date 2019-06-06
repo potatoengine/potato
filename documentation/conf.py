4440f8e23764bd235d4bc908787ab5f863d34bbb
Sean Middleditch <sean.middleditch@gmail.com>
2019-06-05 20:40:07 -0700

    ReadTheDocs scripts/config

--- documentation/conf.py
+++ documentation/conf.py
@@ -54,3 +54,27 @@ html_static_path = ['_static']
 
 # Breath Configuration
 breathe_default_project  = "PotatoEngine"
+breathe_projects = {}
+
+# ReadTheDocs
+import subprocess, os
+
+def configureDoxyfile(input_dir, output_dir):
+    with open('Doxyfile.in', 'r') as file :
+        filedata = file.read()
+
+    filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
+    filedata = filedata.replace('@DOXYGEN_OUTPUT_DIR@', output_dir)
+
+    with open('Doxyfile', 'w') as file:
+        file.write(filedata)
+
+# Check if we're running on Read the Docs' servers
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+if read_the_docs_build:
+    input_dir = '../source'
+    output_dir = 'build'
+    configureDoxyfile(input_dir, output_dir)
+    subprocess.call('doxygen', shell=True)
+    breathe_projects['PotatoEngine'] = output_dir + '/xml'
