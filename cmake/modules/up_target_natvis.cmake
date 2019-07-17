aeb726212b6c422f7e04826c4ac8bf04d1e2d94b
Sean Middleditch <sean.middleditch@gmail.com>
2019-07-16 21:27:20 -0700

    Enable natvis support for glm (#113)
    * Enable natvis support for glm
    
    Had to import a newer glm.natvis as the one in the latest release is outdated and doesn't work
    
    * Add a helper for adding natvis files
    
    * Use up_target_natvis helper for glm and foundation
    
    This fixes the bug in glm on non-Windows platforms
    
    * Build fix for glm natvis
    
    Linking to files in the source directory only works for build, not exported targets

---
+++ cmake/modules/up_target_natvis.cmake
@@ -0,0 +1,21 @@
+function(up_target_natvis TARGET_NAME NATVIS)
+    # determine if target is an INTERFACE library; if so, we add the natvis file
+    # link command as an INTERFACE dependency, otherwise we link it directly
+    get_target_property(target_type "${TARGET_NAME}" TYPE)
+    if(target_type STREQUAL INTERFACE_LIBRARY)
+        set(visibility INTERFACE)
+    else()
+        set(visibility PRIVATE)
+    endif()
+
+    if(${CMAKE_CXX_COMPILER_ID} STREQUAL MSVC AND ${CMAKE_GENERATOR} STREQUAL Ninja)
+        # When building with Ninja, we have to manually pass this to the linker
+        # for MSVC since CMake doesn't do it automatically
+        target_link_options("${TARGET_NAME}" "${visibility}" "/NATVIS:${NATVIS}")
+    else()
+        # For MSVC with MSBuild generator, we can just add the .natvis as a source;
+        # for other platforms and toolsets, this doesn't hurt anything so we don't
+        # need additional checks
+        target_sources("${TARGET_NAME}" "${visibility}" "$<BUILD_INTERFACE:${NATVIS}>")
+    endif()
+endfunction()
---
+++ external/glm_natvis.cmake
@@ -0,0 +1,6 @@
+include(up_target_natvis)
+
+up_target_natvis(glm
+    #"${glm_SOURCE_DIR}/util/glm.natvis"
+    "${CMAKE_CURRENT_SOURCE_DIR}/glm.natvis"
+)
