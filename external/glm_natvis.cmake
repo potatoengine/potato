7da8506f6fbfb4ab36bfc411f4b6f6dfb9620d9e
Sean Middleditch <sean.middleditch@gmail.com>
2019-07-16 21:02:37 -0700

    Use up_target_natvis helper for glm and foundation
    This fixes the bug in glm on non-Windows platforms

--- external/glm_natvis.cmake
+++ external/glm_natvis.cmake
@@ -1,15 +1,6 @@
-if(${CMAKE_CXX_COMPILER_ID} STREQUAL MSVC AND ${CMAKE_GENERATOR} STREQUAL Ninja)
-    # When building with Ninja, we have to manually pass this to the linker
-    # for MSVC since CMake doesn't do it automatically
-    target_link_options(glm INTERFACE
-        #"/NATVIS:${glm_SOURCE_DIR}/util/glm.natvis"
-        "/NATVIS:${CMAKE_CURRENT_SOURCE_DIR}/glm.natvis"
-    )
-else()
-    # For MSVC with MSBuild generator, we can just add the .natvis as a source;
-    # for other platforms and toolsets, this doesn't hurt anything so we don't
-    # need additional checks
-    target_sources(foundation INTERFACE
-        ${CMAKE_CURRENT_SOURCE_DIR}/glm.natvis
-    )
-endif()
+include(up_target_natvis)
+
+up_target_natvis(glm
+    #"${glm_SOURCE_DIR}/util/glm.natvis"
+    "${CMAKE_CURRENT_SOURCE_DIR}/glm.natvis"
+)
