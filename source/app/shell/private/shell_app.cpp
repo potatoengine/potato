9f3601efb914266b3ef581039c6c686727cf3d44
Sean Middleditch <sean.middleditch@gmail.com>
2019-08-17 13:26:21 -0700

    FontAwesome icons (#119)
    * Add FontAwesome fonts
    
    * Support copying .ttf files
    
    Also ignore .txt files (we want to keep licenses near assets)
    
    * Support loading FontAwesome into dear imgui
    
    * Load and use FontAwesome in shell
    
    * Add some more icons
    
    Using a lemon since we don't have a potato icon

--- source/app/shell/private/shell_app.cpp
+++ source/app/shell/private/shell_app.cpp
@@ -137,6 +137,15 @@ int up::ShellApp::initialize() {
     }
 
     _drawImgui.bindShaders(std::move(imguiVertShader), std::move(imguiPixelShader));
+    auto fontStream = _fileSystem.openRead("resources/fonts/fontawesome5/fa-solid-900.ttf");
+    if (!fontStream) {
+        _errorDialog("Failed to open FontAwesome font");
+        return 1;
+    }
+    if (!_drawImgui.loadFontAwesome5(std::move(fontStream))) {
+        _errorDialog("Failed to load FontAwesome font");
+        return 1;
+    }
     _drawImgui.createResources(*_device);
 
     _camera.lookAt({0, 10, 15}, {0, 0, 0}, {0, 1, 0});
@@ -286,14 +295,14 @@ void up::ShellApp::_drawUI() {
     if (ImGui::BeginMainMenuBar()) {
         menuSize = ImGui::GetWindowSize();
 
-        if (ImGui::BeginMenu("Potato")) {
-            if (ImGui::MenuItem("Quit", "ESC")) {
+        if (ImGui::BeginMenu(u8"\uf094 Potato")) {
+            if (ImGui::MenuItem(u8"\uf52b Quit", "ESC")) {
                 _running = false;
             }
             ImGui::EndMenu();
         }
 
-        if (ImGui::BeginMenu("View")) {
+        if (ImGui::BeginMenu(u8"\uf06e View")) {
             if (ImGui::BeginMenu("Camera")) {
                 if (ImGui::MenuItem("Fly", "ctrl-f")) {
                     _cameraController = new_box<FlyCameraController>(_camera);
@@ -316,7 +325,7 @@ void up::ShellApp::_drawUI() {
         ImGui::Spacing();
         ImGui::Spacing();
 
-        if (ImGui::MenuItem(!_paused ? "Pause" : "Play", "F5")) {
+        if (ImGui::MenuItem(!_paused ? u8"\uf04c Pause" : u8"\uf04b Play", "F5")) {
             _paused = !_paused;
         }
 
