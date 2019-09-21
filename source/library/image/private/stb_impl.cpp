f42bc4fcefa2b5147a65094a6161508e86c12a3d
Sean Middleditch <sean.middleditch@gmail.com>
2019-09-21 15:11:51 -0700

    Use -Wno-error=maybe-uninitialized

--- source/library/image/private/stb_impl.cpp
+++ source/library/image/private/stb_impl.cpp
@@ -3,17 +3,5 @@
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_DXT_IMPLEMENTATION
 
-// STB has some code issues
-//
-#if defined(UP_COMPILER_GCC)
-#    pragma GCC diagnostic push
-#    pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
-#    pragma GCC diagnostic ignored "-Wuninitialized"
-#endif
-
 #include <stb_image.h>
 #include <stb_dxt.h>
-
-#if defined(UP_COMPILER_GCC)
-#    pragma GCC diagnostic pop
-#endif
