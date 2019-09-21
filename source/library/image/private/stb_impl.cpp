a3d78e97f447e0ade3c2cc17e9fdb70a99963fab
Sean Middleditch <sean.middleditch@gmail.com>
2019-09-21 10:18:58 -0700

    Try to silence gcc error in stb header

--- source/library/image/private/stb_impl.cpp
+++ source/library/image/private/stb_impl.cpp
@@ -3,5 +3,16 @@
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_DXT_IMPLEMENTATION
 
+// STB has some code issues
+//
+#if UP_COMPILER_GCC
+#   pragma GCC diagnostic push
+#   pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#endif
+
 #include <stb_image.h>
 #include <stb_dxt.h>
+
+#if UP_COMPILER_GCC
+#   pragma GCC diagnostic pop
+#endif
