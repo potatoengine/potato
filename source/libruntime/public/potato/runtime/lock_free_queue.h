7392550c8870aa01d66e4ee21b73c56fcd801982
Sean Middleditch <sean.middleditch@gmail.com>
2019-11-06 00:27:06 -0800

    Attempt fix on GitHub Action error

--- source/libruntime/public/potato/runtime/lock_free_queue.h
+++ source/libruntime/public/potato/runtime/lock_free_queue.h
@@ -11,42 +11,57 @@
 namespace up {
     template <typename T, std::size_t CacheLineWidth = 64>
     struct alignas(CacheLineWidth) AlignedAtomic : std::atomic<T> {
+        using std::atomic<T>::atomic;
         char _padding[CacheLineWidth - sizeof(std::atomic<T>)];
     };
 
-    template <typename T, std::size_t Size = 512, std::size_t CacheLineWidth = 64>
+    template <typename T, std::size_t Capacity = 512, std::size_t CacheLineWidth = 64>
     class LockFreeQueue {
-        static constexpr std::uint32_t kBufferSize = Size;
+        static constexpr std::uint32_t kBufferSize = Capacity;
         static constexpr std::uint32_t kBufferMask = kBufferSize - 1;
 
-        static_assert((kBufferSize & kBufferMask) == 0, "ConcurrentQueue size must be a power of 2");
-
-        AlignedAtomic<std::uint32_t, CacheLineWidth> _sequence[kBufferSize];
-        AlignedAtomic<std::uint32_t, CacheLineWidth> _enque;
-        AlignedAtomic<std::uint32_t, CacheLineWidth> _deque;
-        std::aligned_storage_t<sizeof(T), alignof(T)> _buffer[kBufferSize];
+        static_assert((kBufferSize & kBufferMask) == 0, "LockFreeQueue size must be a power of 2");
 
     public:
-        inline LockFreeQueue() noexcept;
+        LockFreeQueue();
+        ~LockFreeQueue();
+
         LockFreeQueue(LockFreeQueue const&) = delete;
         LockFreeQueue& operator=(LockFreeQueue const&) = delete;
+
+        constexpr auto capacity() const noexcept { return Capacity; }
 
         template <typename InsertT>
         [[nodiscard]] inline bool tryEnque(InsertT&& value);
         [[nodiscard]] inline bool tryDeque(T& out);
+
+    private:
+        AlignedAtomic<std::uint32_t, CacheLineWidth> _enque;
+        AlignedAtomic<std::uint32_t, CacheLineWidth> _deque;
+        AlignedAtomic<std::uint32_t, CacheLineWidth>* _sequence = nullptr;
+        std::aligned_storage_t<sizeof(T), alignof(T)>* _buffer = nullptr;
     };
 
-    template <typename T, std::size_t Size, std::size_t CacheLineWidth>
-    LockFreeQueue<T, Size, CacheLineWidth>::LockFreeQueue() noexcept {
-        _enque.store(0, std::memory_order_relaxed);
-        _deque.store(0, std::memory_order_relaxed);
+    template <typename T, std::size_t Capacity, std::size_t CacheLineWidth>
+    LockFreeQueue<T, Capacity, CacheLineWidth>::LockFreeQueue()
+        : _enque(0)
+        , _deque(0)
+        , _sequence(new AlignedAtomic<std::uint32_t, CacheLineWidth>[kBufferSize])
+        , _buffer(new std::aligned_storage_t<sizeof(T), alignof(T)>[kBufferSize])
+    {
         for (std::uint32_t i = 0; i != kBufferSize; ++i)
             _sequence[i].store(i, std::memory_order_relaxed);
     }
 
-    template <typename T, std::size_t Size, std::size_t CacheLineWidth>
+    template <typename T, std::size_t Capacity, std::size_t CacheLineWidth>
+    LockFreeQueue<T, Capacity, CacheLineWidth>::~LockFreeQueue() {
+        delete[] _sequence;
+        delete[] _buffer;
+    }
+
+    template <typename T, std::size_t Capacity, std::size_t CacheLineWidth>
     template <typename InsertT>
-    bool LockFreeQueue<T, Size, CacheLineWidth>::tryEnque(InsertT&& value) {
+    bool LockFreeQueue<T, Capacity, CacheLineWidth>::tryEnque(InsertT&& value) {
         std::uint32_t target = _enque.load(std::memory_order_relaxed);
         std::uint32_t id = _sequence[target & kBufferMask].load(std::memory_order_acquire);
         std::int32_t delta = id - target;
@@ -66,8 +81,8 @@ namespace up {
         return true;
     }
 
-    template <typename T, std::size_t Size, std::size_t CacheLineWidth>
-    bool LockFreeQueue<T, Size, CacheLineWidth>::tryDeque(T& out) {
+    template <typename T, std::size_t Capacity, std::size_t CacheLineWidth>
+    bool LockFreeQueue<T, Capacity, CacheLineWidth>::tryDeque(T& out) {
         std::uint32_t target = _deque.load(std::memory_order_relaxed);
         std::uint32_t id = _sequence[target & kBufferMask].load(std::memory_order_acquire);
         std::int32_t delta = id - (target + 1);
