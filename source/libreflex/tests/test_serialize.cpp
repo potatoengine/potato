3a50db5f42f8b9aa2eba7422e8b69a2b50ce7f50
Sean Middleditch <sean.middleditch@gmail.com>
2019-11-27 22:58:00 -0800

    Simplify json serialize tests

--- source/libreflex/tests/test_serialize.cpp
+++ source/libreflex/tests/test_serialize.cpp
@@ -22,15 +22,9 @@ namespace {
         int x, y, z;
     };
 
-    struct Bigger {
-        Fields xyz;
-        float num;
-        up::string_view name;
-    };
-
     struct Complex {
         Fields xyz;
-        Fields abc;
+        float num;
         up::string name;
     };
 
@@ -40,15 +34,9 @@ namespace {
         reflect("z", &Fields::z);
     }
 
-    UP_REFLECT_TYPE(Bigger) {
-        reflect("xyz", &Bigger::xyz);
-        reflect("num", &Bigger::num);
-        reflect("name", &Bigger::name);
-    }
-
     UP_REFLECT_TYPE(Complex) {
         reflect("xyz", &Complex::xyz);
-        reflect("abc", &Complex::abc);
+        reflect("num", &Complex::num);
         reflect("name", &Complex::name);
     }
 } // namespace
@@ -57,31 +45,11 @@ DOCTEST_TEST_SUITE("[potato][reflect] serialize") {
     using namespace up;
     using namespace up::reflex;
 
-    DOCTEST_TEST_CASE("serialize int") {
-        int i = 1;
-        RecordingSerializer s{};
-        serialize(i, s);
-    }
-
-    DOCTEST_TEST_CASE("serialize simple struct to json") {
+    DOCTEST_TEST_CASE("serialize struct to json") {
         auto root = nlohmann::json::object();
         auto serializer = JsonStreamSerializer{root};
 
-        auto const xyz = Fields{1, 2, 3};
-
-        serialize(xyz, serializer);
-
-        std::ostringstream ostr;
-        ostr << root;
-
-        DOCTEST_CHECK_EQ(ostr.str(), R"--({"x":1,"y":2,"z":3})--");
-    }
-
-    DOCTEST_TEST_CASE("serialize bigger struct to json") {
-        auto root = nlohmann::json::object();
-        auto serializer = JsonStreamSerializer{root};
-
-        auto const big = Bigger{
+        auto const big = Complex{
             {1, 2, 3},
             42.f,
             "bob"};
@@ -94,48 +62,20 @@ DOCTEST_TEST_SUITE("[potato][reflect] serialize") {
         DOCTEST_CHECK_EQ(ostr.str(), R"--({"name":"bob","num":42.0,"xyz":{"x":1,"y":2,"z":3}})--");
     }
 
-    DOCTEST_TEST_CASE("deserialize simple struct from json") {
-        auto root = nlohmann::json::parse(R"--({"x":1,"y":2,"z":3})--");
-        auto serializer = JsonStreamDeserializer{root};
-
-        Fields xyz;
-        serialize(xyz, serializer);
-
-        DOCTEST_CHECK_EQ(1, xyz.x);
-        DOCTEST_CHECK_EQ(2, xyz.y);
-        DOCTEST_CHECK_EQ(3, xyz.z);
-
-        root = nlohmann::json::parse(R"--({"z":7,"x":3})--");
-
-        serialize(xyz, serializer);
-
-        DOCTEST_CHECK_EQ(3, xyz.x);
-        DOCTEST_CHECK_EQ(2, xyz.y);
-        DOCTEST_CHECK_EQ(7, xyz.z);
-    }
-
-    DOCTEST_TEST_CASE("deserialize bigger struct from json") {
+    DOCTEST_TEST_CASE("deserialize struct from json") {
         auto root = nlohmann::json::parse(R"--({"name":"bob","num":42.0,"xyz":{"x":1,"y":2,"z":3}})--");
         auto serializer = JsonStreamDeserializer{root};
 
-        Bigger big;
+        Complex big;
         serialize(big, serializer);
 
-        DOCTEST_CHECK_EQ(big.xyz.z, 3);
+        DOCTEST_CHECK_EQ(1, big.xyz.x);
+        DOCTEST_CHECK_EQ(2, big.xyz.y);
+        DOCTEST_CHECK_EQ(3, big.xyz.z);
+
         DOCTEST_CHECK_EQ(big.num, 42.f);
-        DOCTEST_CHECK(big.name.empty());
-    }
 
-    DOCTEST_TEST_CASE("deserialize complex struct from json") {
-        auto root = nlohmann::json::parse(R"--({"name":"bob","abc":{"x":-4,"y":-5,"z":-6},"xyz":{"x":1,"y":2,"z":3}})--");
-        auto serializer = JsonStreamDeserializer{root};
-
-        Complex cmp;
-        serialize(cmp, serializer);
-
-        DOCTEST_CHECK_EQ(cmp.xyz.z, 3);
-        DOCTEST_CHECK_EQ(cmp.abc.z, -6);
-        DOCTEST_CHECK_EQ(cmp.name, "bob");
+        DOCTEST_CHECK_EQ(big.name, "bob");
     }
 }
 
