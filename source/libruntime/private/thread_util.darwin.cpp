db63cf32950932af8e65e92ac592632e08db7c27
Sean Middleditch <sean.middleditch@gmail.com>
2020-01-18 23:37:32 -0800

    Remove Apple platform support (#140)
    * Remove Apple platforms from our build matrix
    
    * Remove darwin (Apple) specific source files
    
    * Add clang build configuration on Ubuntu
    
    * Cleanup actions yaml since Apple platform is gone
    
    * correct Actions use of filters
    
    * Use clang 9
    
    * Split apt-get update to a separate step
    
    So it's clear it doesn't need to be repeated

--- source/libruntime/private/thread_util.darwin.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright (C) 2019 Sean Middleditch, all rights reserverd.
-
-#include "potato/runtime/thread_util.h"
-#include <pthread.h>
-
-void up::setCurrentThreadName(zstring_view name) noexcept {
-    pthread_setname_np(name.c_str());
-}
