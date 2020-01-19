30bd9dbc19416309aff4f357d66ffb102add839a
Sean Middleditch <sean.middleditch@gmail.com>
2020-01-19 00:43:37 -0800

    Remove small_string
    It would belong in spud, if we used it at all

--- source/libformat/tests/test_writer.cc
+++ source/libformat/tests/test_writer.cc
@@ -1,6 +1,5 @@
 #include "potato/format/format.h"
 #include "potato/format/std_string.h"
-#include "potato/format/small_string.h"
 #include "potato/format/writers.h"
 #include <doctest/doctest.h>
 #include <vector>
@@ -25,15 +24,15 @@ DOCTEST_TEST_CASE("writer") {
     }
 
     DOCTEST_SUBCASE("buffered") {
-        small_string<char, 4> buf;
-        append_writer writer(buf);
+        std::string tmp;
+        append_writer writer(tmp);
 
         format_to(writer, "1{}3", "2");
-        DOCTEST_CHECK_EQ(std::string("123"), buf.c_str());
+        DOCTEST_CHECK_EQ(std::string("123"), tmp.c_str());
 
-        buf.clear();
+        tmp.clear();
         format_to(writer, "1{}3{}5{}7{}9{}", 2, 4, 6, 8, 0);
-        DOCTEST_CHECK_EQ(std::string("1234567890"), buf.c_str());
+        DOCTEST_CHECK_EQ(std::string("1234567890"), tmp.c_str());
     }
 
     DOCTEST_SUBCASE("container") {
