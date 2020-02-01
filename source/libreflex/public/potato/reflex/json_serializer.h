87f7fc32928746b9c70f0460bd313294dab1cbdf
Sean Middleditch <sean.middleditch@gmail.com>
2020-02-01 15:02:01 -0800

    Reflection and serialization (#111)
    * Stub of reflect library
    
    * Begin trying to support reflection and not just serialization
    
    * Cleanup reflection header
    
    * Beginning of type metadata for runtime use
    
    * Type info definition
    
    * Some work on JSON serializer
    
    * Kill the sample xml serializer
    
    * Fix sub-object JSON serialization
    
    * Example JSON deserialization
    
    * More complex deserialization including strings
    
    This isn't generalized enough for different kinds of writeable strings yet
    
    * Further serialization refinement
    
    * CRTP for serializer
    
    Unsure if this actually helps... yet, at least
    
    * Fix remove_cvref_t and add test
    
    * Use CRTP for JSON serializer too
    
    Now it does at least simplify the code over not having it
    
    * Simplify string handling
    
    * Begin splitting up JSON serializer from core reflect code
    
    * Split traits to their own header
    
    * Clarify the silly symbol we have defined in reflect library
    
    * Rename reflect library to reflex
    
    * Ensure JSON serializers get type infos
    
    * Rename stream_serializer.h
    
    * Start attempting to get reflection into scene
    
    * Move library and update for conventions
    
    * Try to disable an annoying clang warning
    
    * Naming standard for libreflex
    
    Also fix up so interface libraries can use up_set_common_properties
    
    * Simplify json serialize tests
    
    * Horrific start to getter/setter pairs and containers in reflection
    
    This... is not what we want.
    
    ASL overloads up the wazoo.
    Const problems.
    Template explosion.
    
    Ugh.
    
    * Use a common tag type
    
    We've used these elsewhere, no need to proliferate types
    
    * Remove the runtime metadata system
    
    It's... not really usable, and complicates code
    
    * Work vector serialization
    
    * Remove vestigal getter/setter code
    
    Two-phase compilers choke on it
    
    * Make vector initializer_list constructor implicit
    
    Was made explicit by habit; MSVC accepted it, clang has problems with it
    
    * Attempt to disable some GCC warnings in preprocessor test
    
    Shoudl we just remove these utilities? the experiment that required them is long dead
    
    * Remove reflex container.h
    
    Opting out of the general approach for v1
    
    * Avoid infinite recursion on unknown types
    
    * Test that unknown JSON object fields are ignored
    
    * Flatten builtin overloads using a CPO-like model
    
    * [WIP] JSON serializer refactoring
    
    I think we'll need enter/leave field/item after all for efficient JSON decoding
    
    * JSON serialization fixes
    
    * Initial outline of annotation support w/ demo
    
    * GCC fix
    
    * Help the optimizer a little bit
    
    Yes, this actually affected codegen.

---
+++ source/libreflex/public/potato/reflex/json_serializer.h
@@ -0,0 +1,220 @@
+// Copyright (C) 2019 Potato Engine authors and contributors, all rights reserverd.
+
+#pragma once
+
+#include "serializer.h"
+#include "annotation.h"
+#include <potato/spud/string_writer.h>
+#include <potato/spud/string_format.h>
+#include <potato/spud/string.h>
+#include <potato/spud/vector.h>
+#include <nlohmann/json.hpp>
+
+// temporary - FIXME remove
+namespace up {
+    static inline void from_json(const nlohmann::json& json, up::string& str) noexcept {
+        str.reset();
+        if (json.is_string()) {
+            const std::string& stdString = json;
+            str = up::string(stdString.data(), stdString.size());
+        }
+    }
+} // namespace up
+
+namespace up::reflex {
+    struct JsonName {
+        constexpr JsonName(zstring_view n) noexcept : name(n) {}
+
+        zstring_view name;
+    };
+
+    /// Writes JSON from objects
+    class JsonStreamSerializer : public SerializerBase<JsonStreamSerializer> {
+    public:
+        JsonStreamSerializer(nlohmann::json& root) noexcept : _root(root), _current({&_root}) {}
+
+        friend class SerializerBase<JsonStreamSerializer>;
+
+    protected:
+        template <typename T, typename Annotations>
+        Action enterField(tag<T>, zstring_view name, Annotations&& annotations) noexcept {
+            auto extract = [this](auto const& name) { _nextField = name.name; };
+            if (!ApplyAnnotation<JsonName>(annotations, extract)){
+                _nextField = name;
+            }
+            return Action::Enter;
+        }
+
+        template <typename T>
+        Action enterItem(tag<T>, size_t) noexcept {
+            _nextAppend = true;
+            return Action::Enter;
+        }
+
+        template <typename T>
+        Action beginObject(tag<T>) {
+            _current.push_back(_assign(nlohmann::json::object()));
+            return Action::Enter;
+        }
+
+        template <typename T>
+        Action beginArray(tag<T>, size_t size) noexcept {
+            _current.push_back(_assign(nlohmann::json::array()));
+            return Action::Enter;
+        }
+
+        void end() noexcept {
+            _current.pop_back();
+        }
+
+        template <typename T>
+        void handle(T&& value) {
+            _assign(_encode(std::forward<T>(value)));
+        }
+
+    private:
+        template <typename T>
+        auto _encode(T&& value) -> nlohmann::json {
+            using BaseT = remove_cvref_t<T>;
+            if constexpr (std::is_same_v<up::string_view, BaseT>) {
+                return std::string(value.begin(), value.end());
+            }
+            else if constexpr (is_string_v<BaseT>) {
+                return value.c_str();
+            }
+            else if constexpr (is_numeric_v<BaseT>) {
+                return value;
+            }
+            else {
+                return nullptr;
+            }
+        }
+
+        auto _assign(nlohmann::json node) -> nlohmann::json* {
+            auto* parent = _current.back();
+
+            if (_nextField) {
+                auto& child = (*parent)[_nextField.c_str()] = std::move(node);
+                _nextField = {};
+                return &child;
+            }
+            else if (_nextAppend) {
+                parent->push_back(std::move(node));
+                auto& child = parent->back();
+                _nextAppend = false;
+                return &child;
+            }
+            else {
+                *parent = std::move(node);
+                return parent;
+            }
+        }
+
+    private:
+        nlohmann::json& _root;
+        vector<nlohmann::json*> _current;
+        zstring_view _nextField;
+        bool _nextAppend = false;
+    }; // namespace up::reflex
+
+    /// Populates objects from JSON
+    class JsonStreamDeserializer : public SerializerBase<JsonStreamDeserializer> {
+    public:
+        explicit JsonStreamDeserializer(nlohmann::json& root) noexcept : _current({&root}) {}
+
+        friend class SerializerBase<JsonStreamDeserializer>;
+
+    protected:
+        template <typename T, typename Annotations>
+        Action enterField(tag<T>, zstring_view name, Annotations&& annotations) noexcept {
+            zstring_view fieldName = name;
+            auto extract = [&fieldName](auto const& name) { fieldName = name.name; };
+            if (!ApplyAnnotation<JsonName>(annotations, extract)) {
+                fieldName = name;
+            }
+
+            auto* node = _current.back();
+            if (node->is_object() && node->contains(fieldName.c_str())) {
+                _nextField = fieldName;
+                return Action::Enter;
+            }
+            return Action::Skip;
+        }
+
+        template <typename T>
+        Action enterItem(tag<T>, size_t index) noexcept {
+            auto* node = _current.back();
+            if (node->is_array() && index < node->size()) {
+                _nextIndex = index;
+                return Action::Enter;
+            }
+            return Action::Skip;
+        }
+
+        template <typename T>
+        Action beginObject(tag<T>) {
+            auto* node = _enter();
+            if (node != nullptr && node->is_object()) {
+                _current.push_back(node);
+                return Action::Enter;
+            }
+            return Action::Skip;
+        }
+
+        template <typename T>
+        Action beginArray(tag<T>, size_t& size) noexcept {
+            auto* node = _enter();
+            if (node != nullptr && node->is_array()) {
+                _current.push_back(node);
+                size = node->size();
+                return Action::Enter;
+            }
+            return Action::Skip;
+        }
+
+        void end() noexcept {
+            _current.pop_back();
+        }
+
+        template <typename T>
+        void handle(T&& value) {
+            using BaseT = remove_cvref_t<T>;
+            auto* node = _enter();
+            if (node != nullptr) {
+                if constexpr (std::is_same_v<BaseT, up::string>) {
+                    if (node->is_string()) {
+                        value = node->get<up::string>();
+                    }
+                }
+                else if constexpr (is_numeric_v<BaseT>) {
+                    if (node->is_number()) {
+                        value = node->get<BaseT>();
+                    }
+                }
+            }
+        }
+
+    private:
+        auto _enter() noexcept -> nlohmann::json* {
+            auto* parent = _current.back();
+            if (_nextField && parent->is_object() && parent->contains(_nextField.c_str())) {
+                auto& current = (*parent)[_nextField.c_str()];
+                _nextField = {};
+                return &current;
+            }
+            else if (parent->is_array() && _nextIndex < parent->size()) {
+                auto& current = (*parent)[_nextIndex];
+                _nextIndex = ~size_t{};
+                return &current;
+            }
+            else {
+                return nullptr;
+            }
+        }
+
+    private:
+        vector<nlohmann::json*> _current;
+        zstring_view _nextField;
+        size_t _nextIndex = ~size_t{};
+    };
+} // namespace up::reflex
---
+++ source/libreflex/tests/test_serialize.cpp
@@ -0,0 +1,104 @@
+#include "potato/reflex/reflect.h"
+#include "potato/reflex/serializer.h"
+#include "potato/reflex/json_serializer.h"
+#include "potato/spud/vector.h"
+#include <doctest/doctest.h>
+#include <sstream>
+
+namespace {
+    struct RecordingSerializer {
+        template <typename T>
+        constexpr void value(T&) noexcept {}
+    };
+
+    struct RecordingReflector {
+        template <typename T>
+        constexpr void value() noexcept {}
+
+        template <typename T, typename F>
+        constexpr void field(up::zstring_view, F T::*) noexcept {}
+    };
+
+    struct Fields {
+        int x, y, z;
+    };
+
+    struct Complex {
+        Fields xyz;
+        float num = 0;
+        up::string name;
+        up::vector<int> vec;
+    };
+
+    UP_REFLECT_TYPE(Fields) {
+        reflect("x", &Fields::x);
+        reflect("y", &Fields::y);
+        reflect("z", &Fields::z);
+    }
+
+    UP_REFLECT_TYPE(Complex) {
+        reflect("xyz", &Complex::xyz);
+        reflect("num", &Complex::num, up::reflex::JsonName("custom"));
+        reflect("name", &Complex::name);
+        reflect("vec", &Complex::vec);
+    }
+} // namespace
+
+DOCTEST_TEST_SUITE("[potato][reflect] serialize") {
+    using namespace up;
+    using namespace up::reflex;
+
+    DOCTEST_TEST_CASE("serialize struct to json") {
+        auto root = nlohmann::json::object();
+        auto serializer = JsonStreamSerializer{root};
+
+        auto const big = Complex{
+            {1, 2, 3},
+            42.f,
+            "bob",
+            {4, 5, 6}};
+
+        serialize(big, serializer);
+
+        std::ostringstream ostr;
+        ostr << root;
+
+        DOCTEST_CHECK_EQ(ostr.str(), R"--({"custom":42.0,"name":"bob","vec":[4,5,6],"xyz":{"x":1,"y":2,"z":3}})--");
+    }
+
+    DOCTEST_TEST_CASE("deserialize struct from json") {
+        auto root = nlohmann::json::parse(R"--({"name":"bob","custom":42.0,"ignore":{"num":7},"xyz":{"x":1,"y":2,"z":3},"vec":[4,6]})--");
+        auto serializer = JsonStreamDeserializer{root};
+
+        Complex big;
+        serialize(big, serializer);
+
+        DOCTEST_CHECK_EQ(1, big.xyz.x);
+        DOCTEST_CHECK_EQ(2, big.xyz.y);
+        DOCTEST_CHECK_EQ(3, big.xyz.z);
+
+        DOCTEST_CHECK_EQ(big.num, 42.f);
+
+        DOCTEST_CHECK_EQ(big.name, "bob");
+
+        DOCTEST_CHECK_EQ(big.vec.size(), 2);
+        DOCTEST_CHECK_EQ(big.vec.front(), 4);
+        DOCTEST_CHECK_EQ(big.vec.back(), 6);
+    }
+}
+
+DOCTEST_TEST_SUITE("[potato][reflect] reflect") {
+    using namespace up;
+    using namespace up::reflex;
+
+    DOCTEST_TEST_CASE("reflect int") {
+        RecordingReflector ref{};
+        reflect<int>(ref);
+    }
+
+    DOCTEST_TEST_CASE("reflect struct") {
+        RecordingReflector ref{};
+
+        reflect<Fields>(ref);
+    }
+}
