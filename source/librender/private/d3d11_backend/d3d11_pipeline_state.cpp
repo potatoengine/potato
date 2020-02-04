64b5c70a3514303ade1362e875646fae8836672b
Sean Middleditch <sean.middleditch@gmail.com>
2020-02-03 22:59:42 -0800

    Fix default alpha blending to be src[alpha]

--- source/librender/private/d3d11_backend/d3d11_pipeline_state.cpp
+++ source/librender/private/d3d11_backend/d3d11_pipeline_state.cpp
@@ -35,7 +35,7 @@ auto up::d3d11::PipelineStateD3D11::createGraphicsPipelineState(GpuPipelineState
     blendDesc.RenderTarget[0].SrcBlend = D3D11_BLEND_SRC_ALPHA;
     blendDesc.RenderTarget[0].DestBlend = D3D11_BLEND_INV_SRC_ALPHA;
     blendDesc.RenderTarget[0].BlendOp = D3D11_BLEND_OP_ADD;
-    blendDesc.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_INV_SRC_ALPHA;
+    blendDesc.RenderTarget[0].SrcBlendAlpha = D3D11_BLEND_SRC_ALPHA;
     blendDesc.RenderTarget[0].DestBlendAlpha = D3D11_BLEND_ZERO;
     blendDesc.RenderTarget[0].BlendOpAlpha = D3D11_BLEND_OP_ADD;
     blendDesc.RenderTarget[0].RenderTargetWriteMask = D3D11_COLOR_WRITE_ENABLE_ALL;
