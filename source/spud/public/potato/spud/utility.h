0b0bf9d12ac4331768506359c7973a8decde6d47
Sean Middleditch <sean.middleditch@gmail.com>
2020-05-02 16:29:49 -0700

    spud's sequence type has a traits parameter can handles scoped enums better

--- source/spud/public/potato/spud/utility.h
+++ source/spud/public/potato/spud/utility.h
@@ -82,7 +82,26 @@ namespace up {
     template <typename T>
     struct tag {};
 
+    namespace _detail {
+        template <typename T, bool E = false>
+        struct default_sequence_traits {
+            static constexpr auto increment(T& value) noexcept -> T& { return ++value; }
+            static constexpr auto equal(T start, T end) noexcept -> bool { return start == end; }
+            static constexpr auto difference(T start, T end) noexcept -> size_t { return end - start; }
+        };
+
+        template <typename T>
+        struct default_sequence_traits<T, true> {
+            static constexpr auto increment(T& value) noexcept -> T& { return value = static_cast<T>(++to_underlying(value)); }
+            static constexpr auto equal(T start, T end) noexcept -> bool { return start == end; }
+            static constexpr auto difference(T start, T end) noexcept -> size_t { return to_underlying(end) - to_underlying(start); }
+        };
+    } // namespace _detail
+
     template <typename T>
+    using sequence_traits = _detail::default_sequence_traits<T, std::is_enum_v<T>>;
+
+    template <typename T, typename Traits = sequence_traits<T>>
     class sequence {
     public:
         struct sentinel {};
@@ -94,13 +113,11 @@ namespace up {
             constexpr auto operator*() noexcept { return _value; }
 
             constexpr auto operator++() noexcept -> iterator& {
-                ++_value;
+                Traits::increment(_value);
                 return *this;
             }
 
-            constexpr auto operator!=(sentinel) const noexcept {
-                return _value != _end;
-            }
+            constexpr auto operator!=(sentinel) const noexcept { return !Traits::equal(_value, _end); }
 
         private:
             T _value = {};
@@ -108,10 +125,16 @@ namespace up {
         };
 
         constexpr explicit sequence(T end) noexcept : _end(end) {}
-        constexpr explicit sequence(T start, T end) noexcept : _start(start), _end(end) {}
+        constexpr sequence(T start, T end) noexcept : _start(start), _end(end) {}
 
         constexpr auto begin() const noexcept -> iterator { return iterator{_start, _end}; }
         constexpr auto end() const noexcept -> sentinel { return {}; }
+
+        constexpr explicit operator bool() const noexcept { return !Traits::equal(_start, _end); }
+
+        constexpr bool empty() const noexcept { return Traits::equal(_start, _end); }
+
+        constexpr auto size() const noexcept { return Traits::difference(_start, _end); }
 
     private:
         T _start = {};
