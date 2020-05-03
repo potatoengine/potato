64681fd586ec7b3135712bf82b9d6d22409a3048
Sean Middleditch <sean.middleditch@gmail.com>
2020-05-02 19:13:09 -0700

    Cleanup (#153)
    * Doxygen settings in editorconfig
    
    * Some example Doxygen docs in panel.h
    
    * Disable a rather spurious warning in clang-tidy
    
    As with many of these static analysis checks, they have no idea about the "concept" in play and will recommend that `auto` be `auto*` just because a container uses raw pointers as an iterator type, which would defeat the purpose of using type inference in the first place.
    
    * Disable static analysis on CMakeSettings.json
    
    Mostly spurious still, we need to figure out the real set of settings we want (and figure out how to make it only apply to our code)

--- source/bin_shell/public/potato/shell/panel.h
+++ source/bin_shell/public/potato/shell/panel.h
@@ -17,10 +17,17 @@ namespace up::shell {
         Panel(Panel const&) = delete;
         Panel& operator=(Panel const&) = delete;
 
+        /// @brief Display name of the panel, used in menus and window titles.
+        /// @return display name.
         virtual zstring_view displayName() const = 0;
+
+        /// @brief Renders the ui for the panel. Only called when the panel is enabled.
         virtual void ui() = 0;
 
+        /// @return true if the panel is enabled (visible).
         bool enabled() const noexcept { return _enabled; }
+
+        /// @param enabled Whether the panel should be enabled or disabled.
         void enabled(bool enabled) noexcept { _enabled = enabled; }
 
     protected:
