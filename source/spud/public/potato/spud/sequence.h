b626dd1401c69f0d5553f8e317fdade855c0b493
Sean Middleditch <sean.middleditch@gmail.com>
2020-05-21 21:48:53 -0700

    Remove sequence_traits
    Simplify how we handle enums.
    
    Assume any other custom types will just behave like integers.

--- source/spud/public/potato/spud/sequence.h
+++ source/spud/public/potato/spud/sequence.h
@@ -2,27 +2,10 @@
 
 #pragma once
 
+#include <type_traits>
+
 namespace up {
-    namespace _detail {
-        template <typename T, bool E = false>
-        struct default_sequence_traits {
-            static constexpr auto increment(T& value) noexcept -> T& { return ++value; }
-            static constexpr auto equal(T start, T end) noexcept -> bool { return start == end; }
-            static constexpr auto difference(T start, T end) noexcept -> size_t { return end - start; }
-        };
-
-        template <typename T>
-        struct default_sequence_traits<T, true> {
-            static constexpr auto increment(T& value) noexcept -> T& { return value = static_cast<T>(++to_underlying(value)); }
-            static constexpr auto equal(T start, T end) noexcept -> bool { return start == end; }
-            static constexpr auto difference(T start, T end) noexcept -> size_t { return to_underlying(end) - to_underlying(start); }
-        };
-    } // namespace _detail
-
     template <typename T>
-    using sequence_traits = _detail::default_sequence_traits<T, std::is_enum_v<T>>;
-
-    template <typename T, typename Traits = sequence_traits<T>>
     class sequence {
     public:
         struct sentinel {};
@@ -31,19 +14,26 @@ namespace up {
         public:
             constexpr explicit iterator(T value, T end) noexcept : _value(value), _end(end) {}
 
-            constexpr auto operator*() noexcept { return _value; }
+            constexpr auto operator*() const noexcept { return _value; }
 
-            constexpr auto operator++() noexcept -> iterator& {
-                Traits::increment(_value);
+            constexpr auto operator++() noexcept -> iterator& requires enumeration<T> {
+                _value = static_cast<T>(++static_cast<std::underlying_type_t<T>>(_value));
                 return *this;
             }
 
-            constexpr auto operator!=(sentinel) const noexcept { return !Traits::equal(_value, _end); }
+            constexpr auto operator++() noexcept -> iterator& {
+                ++_value;
+                return *this;
+            }
+
+            constexpr auto operator!=(sentinel) const noexcept { return _value != _end; }
 
         private:
             T _value = {};
             T _end = {};
         };
+
+        using value_type = T;
 
         constexpr explicit sequence(T end) noexcept : _end(end) {}
         constexpr sequence(T start, T end) noexcept : _start(start), _end(end) {}
@@ -51,11 +41,12 @@ namespace up {
         constexpr auto begin() const noexcept -> iterator { return iterator{_start, _end}; }
         constexpr auto end() const noexcept -> sentinel { return {}; }
 
-        constexpr explicit operator bool() const noexcept { return !Traits::equal(_start, _end); }
+        constexpr explicit operator bool() const noexcept { return !_start != _end; }
 
-        constexpr bool empty() const noexcept { return Traits::equal(_start, _end); }
+        constexpr bool empty() const noexcept { return _start == _end; }
 
-        constexpr auto size() const noexcept { return Traits::difference(_start, _end); }
+        constexpr auto size() const noexcept requires enumeration<T> { return static_cast<std::underlying_type_t<T>>(_end) - static_cast<std::underlying_type_t<T>>(_start); }
+        constexpr auto size() const noexcept { return _end - _start; }
 
     private:
         T _start = {};
