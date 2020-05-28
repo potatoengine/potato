5be9149f21739d22c71d0986c26ae72ad879d6a0
Sean Middleditch <sean.middleditch@gmail.com>
2020-05-27 21:21:49 -0700

    Fix indentation in soloud.cmake

--- external/soloud.cmake
+++ external/soloud.cmake
@@ -16,21 +16,21 @@ target_sources(soloud PRIVATE
     ${soloud_SOURCE_DIR}/src/core/soloud_audiosource.cpp
     ${soloud_SOURCE_DIR}/src/core/soloud_bus.cpp
     ${soloud_SOURCE_DIR}/src/core/soloud_core_3d.cpp
-	${soloud_SOURCE_DIR}/src/core/soloud_core_basicops.cpp
-	${soloud_SOURCE_DIR}/src/core/soloud_core_faderops.cpp
-	${soloud_SOURCE_DIR}/src/core/soloud_core_filterops.cpp
-	${soloud_SOURCE_DIR}/src/core/soloud_core_getters.cpp
-	${soloud_SOURCE_DIR}/src/core/soloud_core_setters.cpp
-	${soloud_SOURCE_DIR}/src/core/soloud_core_voicegroup.cpp
-	${soloud_SOURCE_DIR}/src/core/soloud_core_voiceops.cpp
-	${soloud_SOURCE_DIR}/src/core/soloud_fader.cpp
-	${soloud_SOURCE_DIR}/src/core/soloud_fft.cpp
-	${soloud_SOURCE_DIR}/src/core/soloud_fft_lut.cpp
-	${soloud_SOURCE_DIR}/src/core/soloud_file.cpp
-	${soloud_SOURCE_DIR}/src/core/soloud_filter.cpp
-	${soloud_SOURCE_DIR}/src/core/soloud_misc.cpp
-	${soloud_SOURCE_DIR}/src/core/soloud_queue.cpp
-	${soloud_SOURCE_DIR}/src/core/soloud_thread.cpp
+    ${soloud_SOURCE_DIR}/src/core/soloud_core_basicops.cpp
+    ${soloud_SOURCE_DIR}/src/core/soloud_core_faderops.cpp
+    ${soloud_SOURCE_DIR}/src/core/soloud_core_filterops.cpp
+    ${soloud_SOURCE_DIR}/src/core/soloud_core_getters.cpp
+    ${soloud_SOURCE_DIR}/src/core/soloud_core_setters.cpp
+    ${soloud_SOURCE_DIR}/src/core/soloud_core_voicegroup.cpp
+    ${soloud_SOURCE_DIR}/src/core/soloud_core_voiceops.cpp
+    ${soloud_SOURCE_DIR}/src/core/soloud_fader.cpp
+    ${soloud_SOURCE_DIR}/src/core/soloud_fft.cpp
+    ${soloud_SOURCE_DIR}/src/core/soloud_fft_lut.cpp
+    ${soloud_SOURCE_DIR}/src/core/soloud_file.cpp
+    ${soloud_SOURCE_DIR}/src/core/soloud_filter.cpp
+    ${soloud_SOURCE_DIR}/src/core/soloud_misc.cpp
+    ${soloud_SOURCE_DIR}/src/core/soloud_queue.cpp
+    ${soloud_SOURCE_DIR}/src/core/soloud_thread.cpp
 )
 target_sources(soloud PRIVATE
     ${soloud_SOURCE_DIR}/src/backend/null/soloud_null.cpp
