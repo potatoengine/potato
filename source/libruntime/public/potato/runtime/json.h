df5a360a82ed326f476b67e12741d4dddbbcc12b
Sean Middleditch <sean.middleditch@gmail.com>
2020-05-30 18:58:23 -0700

    Fix some erroneous runtime API declarations

--- source/libruntime/public/potato/runtime/json.h
+++ source/libruntime/public/potato/runtime/json.h
@@ -12,10 +12,10 @@ namespace up {
     class string;
     class Stream;
 
-    extern auto UP_RUNTIME_API readJson(Stream& stream, nlohmann::json& json) -> IOResult;
+    extern UP_RUNTIME_API auto readJson(Stream& stream, nlohmann::json& json) -> IOResult;
 
     // nlohmann overloads
-    extern void UP_RUNTIME_API to_json(nlohmann::json& json, string_view str) noexcept;
-    extern void UP_RUNTIME_API to_json(nlohmann::json& json, string const& str) noexcept;
-    extern void UP_RUNTIME_API from_json(const nlohmann::json& json, string& str) noexcept;
+    extern UP_RUNTIME_API void to_json(nlohmann::json& json, string_view str) noexcept;
+    extern UP_RUNTIME_API void to_json(nlohmann::json& json, string const& str) noexcept;
+    extern UP_RUNTIME_API void from_json(const nlohmann::json& json, string& str) noexcept;
 } // namespace up
