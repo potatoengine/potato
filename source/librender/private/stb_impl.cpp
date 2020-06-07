30e5e6a1f2df505762d6359b94ab05ede918b170
Sean Middleditch <sean.middleditch@gmail.com>
2020-06-06 20:43:34 -0700

    Silence some annoying warnings

--- source/librender/private/stb_impl.cpp
+++ source/librender/private/stb_impl.cpp
@@ -1,7 +1,5 @@
 // Copyright by Potato Engine contributors. See accompanying License.txt for copyright details.
 
 #define STB_IMAGE_IMPLEMENTATION
-#define STB_DXT_IMPLEMENTATION
 
-#include <stb_dxt.h>
 #include <stb_image.h>
