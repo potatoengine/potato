c9662359edab2ce9d175f777dab6f1cadd5d3eea
Sean Middleditch <sean.middleditch@gmail.com>
2020-07-06 21:25:11 -0700

    Convert up::FileSystem to a nested namespace

--- source/libruntime/public/potato/runtime/filesystem.h
+++ source/libruntime/public/potato/runtime/filesystem.h
@@ -11,33 +11,27 @@
 
 namespace up {
     class Stream;
-
-    class FileSystem {
-        FileSystem() = default;
-
-    public:
-        FileSystem(FileSystem const&) = delete;
-        FileSystem& operator=(FileSystem const&) = delete;
-
-        static UP_RUNTIME_API bool fileExists(zstring_view path) noexcept;
-        static UP_RUNTIME_API bool directoryExists(zstring_view path) noexcept;
-
-        static UP_RUNTIME_API IOResult fileStat(zstring_view path, FileStat& outInfo);
-
-        static UP_RUNTIME_API Stream openRead(zstring_view path, FileOpenMode mode = FileOpenMode::Binary);
-        static UP_RUNTIME_API Stream openWrite(zstring_view path, FileOpenMode mode = FileOpenMode::Binary);
-
-        static UP_RUNTIME_API EnumerateResult enumerate(zstring_view path, EnumerateCallback cb, EnumerateOptions opts = EnumerateOptions::None);
-
-        static UP_RUNTIME_API IOResult createDirectories(zstring_view path);
-
-        static UP_RUNTIME_API IOResult remove(zstring_view path);
-        static UP_RUNTIME_API IOResult removeRecursive(zstring_view path);
-
-        static UP_RUNTIME_API string currentWorkingDirectory() noexcept;
-        static UP_RUNTIME_API bool currentWorkingDirectory(zstring_view path);
-
-        static UP_RUNTIME_API IOResult copyFileTo(zstring_view fromPath, zstring_view toPath);
-        static UP_RUNTIME_API IOResult moveFileTo(zstring_view fromPath, zstring_view toPath);
-    };
 } // namespace up
+
+namespace up::FileSystem {
+    UP_RUNTIME_API bool fileExists(zstring_view path) noexcept;
+    UP_RUNTIME_API bool directoryExists(zstring_view path) noexcept;
+
+    UP_RUNTIME_API IOResult fileStat(zstring_view path, FileStat& outInfo);
+
+    UP_RUNTIME_API Stream openRead(zstring_view path, FileOpenMode mode = FileOpenMode::Binary);
+    UP_RUNTIME_API Stream openWrite(zstring_view path, FileOpenMode mode = FileOpenMode::Binary);
+
+    UP_RUNTIME_API EnumerateResult enumerate(zstring_view path, EnumerateCallback cb, EnumerateOptions opts = EnumerateOptions::None);
+
+    UP_RUNTIME_API IOResult createDirectories(zstring_view path);
+
+    UP_RUNTIME_API IOResult remove(zstring_view path);
+    UP_RUNTIME_API IOResult removeRecursive(zstring_view path);
+
+    UP_RUNTIME_API string currentWorkingDirectory() noexcept;
+    UP_RUNTIME_API bool currentWorkingDirectory(zstring_view path);
+
+    UP_RUNTIME_API IOResult copyFileTo(zstring_view fromPath, zstring_view toPath);
+    UP_RUNTIME_API IOResult moveFileTo(zstring_view fromPath, zstring_view toPath);
+} // namespace up::FileSystem
