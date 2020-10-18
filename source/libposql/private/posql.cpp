142b841ab9e839446bcb308ae7a0cc8f0b1ddafb
Sean Middleditch <sean.middleditch@gmail.com>
2020-10-18 16:08:27 -0700

    Flesh out more of posql

--- source/libposql/private/posql.cpp
+++ source/libposql/private/posql.cpp
@@ -2,14 +2,14 @@
 
 #include "posql.h"
 
-#include "potato/spud/out_ptr.h"
+#include "potato/runtime/assertion.h"
 
 #include <sqlite3.h>
 
 up::SqlResult up::Database::open(zstring_view file_name) noexcept {
     close();
 
-    auto const rs = sqlite3_open(file_name.c_str(), out_ptr(_conn));
+    auto const rs = sqlite3_open(file_name.c_str(), &_conn);
     if (rs != SQLITE_OK)
         return SqlResult::Error;
 
@@ -17,23 +17,85 @@ up::SqlResult up::Database::open(zstring_view file_name) noexcept {
 }
 
 void up::Database::close() noexcept {
-    _conn.reset();
+    sqlite3_close(_conn);
+    _conn = nullptr;
 }
 
 up::Statement up::Database::prepare(zstring_view sql) noexcept {
-    SqliteStatement stmt;
-    auto const rs = sqlite3_prepare_v3(_conn.get(), sql.c_str(), -1, 0, out_ptr(stmt), nullptr);
+    sqlite3_stmt* stmt = nullptr;
+    auto const rs = sqlite3_prepare_v3(_conn, sql.c_str(), -1, 0, &stmt, nullptr);
     if (rs != SQLITE_OK)
         return {};
-    return Statement(std::move(stmt));
+    return Statement(stmt);
 }
 
-up::Statement::~Statement() noexcept = default;
-
-up::SqlResult up::Statement::execute() noexcept {
-    if (sqlite3_reset(_stmt.get()) != SQLITE_OK)
-        return SqlResult::Error;
-    if (sqlite3_step(_stmt.get()) != SQLITE_DONE)
+up::SqlResult up::Database::execute(zstring_view sql) noexcept {
+    auto const rs = sqlite3_exec(_conn, sql.c_str(), nullptr, nullptr, nullptr);
+    if (rs != SQLITE_OK)
         return SqlResult::Error;
     return SqlResult::Ok;
+}
+
+up::Statement::~Statement() noexcept {
+    sqlite3_finalize(_stmt);
+}
+
+up::SqlResult up::Statement::execute() noexcept {
+    UP_ASSERT(!sqlite3_stmt_busy(_stmt));
+    if (sqlite3_reset(_stmt) != SQLITE_OK)
+        return SqlResult::Error;
+    if (sqlite3_step(_stmt) != SQLITE_DONE)
+        return SqlResult::Error;
+    return SqlResult::Ok;
+}
+
+up::Query up::Statement::query() noexcept {
+    UP_ASSERT(sqlite3_stmt_busy(_stmt) == 0);
+    sqlite3_step(_stmt);
+    return Query(_stmt);
+}
+
+up::SqlResult up::Statement::bind(int index, int64 value) noexcept {
+    auto const rs = sqlite3_bind_int64(_stmt, index + 1, value);
+    if (rs != SQLITE_OK)
+        return SqlResult::Error;
+    return SqlResult::Ok;
+}
+
+up::SqlResult up::Statement::bind(int index, zstring_view value) noexcept {
+    auto const rs = sqlite3_bind_text(_stmt, index + 1, value.c_str(), -1, nullptr);
+    if (rs != SQLITE_OK)
+        return SqlResult::Error;
+    return SqlResult::Ok;
+}
+
+bool up::Query::iterator::operator==(Query::sentinel) noexcept {
+    return sqlite3_stmt_busy(_stmt) == 0;
+}
+
+up::Query::iterator& up::Query::iterator::operator++() noexcept {
+    for (;;) {
+        auto const rs = sqlite3_step(_stmt);
+        if (rs == SQLITE_OK) {
+            break;
+        }
+        else if (rs == SQLITE_BUSY) {
+            continue;
+        }
+        else {
+            break;
+        }
+    }
+    return *this;
+}
+up::Row up::Query::iterator::operator*() noexcept {
+    return Row{_stmt};
+}
+
+up::int64 up::Row::get_int64(int index) noexcept {
+    return sqlite3_column_int64(_stmt, index);
+}
+
+up::zstring_view up::Row::get_string(int index) noexcept {
+    return reinterpret_cast<char const*>(sqlite3_column_text(_stmt, index));
 }
--- source/libposql/public/potato/posql/posql.h
+++ source/libposql/public/potato/posql/posql.h
@@ -2,26 +2,21 @@
 
 #include "_export.h"
 
-#include "potato/spud/unique_resource.h"
 #include "potato/spud/zstring_view.h"
 
 extern "C" {
 struct sqlite3;
 struct sqlite3_stmt;
-int sqlite3_close_v2(sqlite3*);
-int sqlite3_finalize(sqlite3_stmt*);
 }
 
 namespace up {
     inline namespace posql {
-
-        using SqliteConnection = up::unique_resource<sqlite3*, sqlite3_close_v2>;
-        using SqliteStatement = up::unique_resource<sqlite3_stmt*, sqlite3_finalize>;
-
         enum class SqlResult { Ok = 0, Error };
 
         class Database;
         class Statement;
+        class Query;
+        class Row;
 
         class Database {
         public:
@@ -38,15 +33,16 @@ namespace up {
             [[nodiscard]] explicit operator bool() const noexcept { return _conn != nullptr; }
 
             UP_POSQL_API [[nodiscard]] Statement prepare(zstring_view sql) noexcept;
+            UP_POSQL_API [[nodiscard]] SqlResult execute(zstring_view sql) noexcept;
 
         private:
-            SqliteConnection _conn;
+            sqlite3* _conn = nullptr;
         };
 
         class Statement {
         public:
             Statement() noexcept = default;
-            explicit Statement(SqliteStatement stmt) noexcept : _stmt(std::move(stmt)) {}
+            explicit Statement(sqlite3_stmt* stmt) noexcept : _stmt(stmt) {}
             UP_POSQL_API ~Statement() noexcept;
 
             Statement(Statement const&) = delete;
@@ -56,9 +52,62 @@ namespace up {
             [[nodiscard]] explicit operator bool() const noexcept { return _stmt != nullptr; }
 
             UP_POSQL_API [[nodiscard]] SqlResult execute() noexcept;
+            UP_POSQL_API [[nodiscard]] Query query() noexcept;
+
+            UP_POSQL_API SqlResult bind(int index, int64 value) noexcept;
+            UP_POSQL_API SqlResult bind(int index, zstring_view value) noexcept;
 
         private:
-            SqliteStatement _stmt;
+            sqlite3_stmt* _stmt = nullptr;
+        };
+
+        class Query {
+        public:
+            using value_type = Row;
+            class iterator;
+            struct sentinel {};
+
+            ~Query() noexcept = default;
+
+            inline iterator begin() noexcept;
+            sentinel end() noexcept { return {}; }
+
+        private:
+            explicit Query(sqlite3_stmt* stmt) : _stmt(stmt) {}
+
+            sqlite3_stmt* _stmt = nullptr;
+
+            friend iterator;
+            friend Statement;
+        };
+
+        class Query::iterator {
+        public:
+            UP_POSQL_API [[nodiscard]] bool operator==(Query::sentinel) noexcept;
+            UP_POSQL_API iterator& operator++() noexcept;
+            UP_POSQL_API [[nodiscard]] Row operator*() noexcept;
+
+        private:
+            explicit iterator(sqlite3_stmt* stmt) : _stmt(stmt) {}
+
+            sqlite3_stmt* _stmt = nullptr;
+
+            friend Query;
+        };
+
+        Query::iterator Query::begin() noexcept { return iterator(_stmt); }
+
+        class Row {
+        public:
+            UP_POSQL_API [[nodiscard]] int64 get_int64(int index) noexcept;
+            UP_POSQL_API [[nodiscard]] zstring_view get_string(int index) noexcept;
+
+        private:
+            explicit Row(sqlite3_stmt* stmt) : _stmt(stmt) {}
+
+            friend Query::iterator;
+
+            sqlite3_stmt* _stmt = nullptr;
         };
 
     } // namespace posql
