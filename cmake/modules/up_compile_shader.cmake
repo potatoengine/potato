33bf932dc5512e88077f957b9ad5924c11cca218
Sean Middleditch <sean.middleditch@gmail.com>
2020-11-09 20:08:06 -0800

    Cleanup up_compile_shader function
    Also prepare it for eventual use of GLSL

--- cmake/modules/up_compile_shader.cmake
+++ cmake/modules/up_compile_shader.cmake
@@ -1,5 +1,29 @@
 function(up_compile_shader TARGET)
-    cmake_parse_arguments(ARG "" "PROFILE;ENTRY;SHADER;NAME" "" ${ARGN})
+    cmake_parse_arguments(ARG "" "STAGE;PROFILE;ENTRY;HLSL;NAME" "" ${ARGN})
+
+    if(NOT ARG_STAGE)
+        message(FATAL_ERROR "up_compiler_shader requires STAGE parameter")
+    endif()
+    if(NOT ARG_HLSL)
+        message(FATAL_ERROR "up_compiler_shader requires HLSL parameter")
+    endif()
+
+    if(NOT ARG_PROFILE)
+        if(ARG_STAGE STREQUAL "pixel")
+            set(ARG_PROFILE ps_5_0)
+        else()
+            set(ARG_PROFILE vs_5_0)
+        endif()
+    endif()
+
+    if(NOT ARG_ENTRY)
+        set(ARG_ENTRY "${ARG_STAGE}_main")
+    endif()
+
+    if(NOT ARG_NAME)
+        get_filename_component(BASENAME "${ARG_HLSL}" NAME_WE)
+        set(ARG_NAME "${BASENAME}_${ARG_STAGE}")
+    endif()
 
     if(WIN32)
         find_package(DXC REQUIRED)
@@ -9,8 +33,8 @@ function(up_compile_shader TARGET)
         file(MAKE_DIRECTORY "${OUT_HEADER_DIR}")
         add_custom_command(
             OUTPUT "${OUT_HEADER}"
-            COMMAND dxc::fxc /nologo /O3 /Zi /WX /T "${ARG_PROFILE}" /E "${ARG_ENTRY}" /Fh "${OUT_HEADER}" "${ARG_SHADER}"
-            MAIN_DEPENDENCY "${IMGUI_SHADER_HLSL}"
+            COMMAND dxc::fxc /nologo /O3 /Zi /WX /T "${ARG_PROFILE}" /E "${ARG_ENTRY}" /Fh "${OUT_HEADER}" "${ARG_HLSL}"
+            MAIN_DEPENDENCY "${ARG_HLSL}"
             WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
             COMMAND_EXPAND_LISTS
         )
