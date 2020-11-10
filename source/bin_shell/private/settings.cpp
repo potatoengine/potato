f00200124894c46a4e691758ced34ce6754abdcd
Sean Middleditch <sean.middleditch@gmail.com>
2020-11-10 08:58:08 -0800

    Add editor settings tools

---
+++ source/bin_shell/private/settings.cpp
@@ -0,0 +1,23 @@
+// Copyright by Potato Engine contributors. See accompanying License.txt for copyright details.
+
+#include "settings.h"
+
+#include "potato/runtime/filesystem.h"
+#include "potato/runtime/json.h"
+
+#include <nlohmann/json.hpp>
+
+bool up::shell::loadShellSettings(zstring_view filename, schema::EditorSettings& settings) {
+    if (auto [rs, doc] = readJson(filename); rs == IOResult::Success) {
+        from_json(doc, settings);
+        return true;
+    }
+
+    return false;
+}
+bool up::shell::saveShellSettings(zstring_view filename, schema::EditorSettings const& settings) {
+    nlohmann::json doc;
+    to_json(doc, settings);
+    auto const rs = fs::writeAllText(filename, doc.dump());
+    return rs == IOResult::Success;
+}
