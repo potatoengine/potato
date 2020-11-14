f99b5b3bc4de1a84fd200eb7ab4827793ca0397b
Marcin Wieczorek <marcinwie@gmail.com>
2020-11-14 14:01:02 -0800

    deleting dx11 files

--- source/librender/private/d3d11_backend/d3d11_resource_view.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright by Potato Engine contributors. See accompanying License.txt for copyright details.
-
-#include "d3d11_resource_view.h"
-
-up::d3d11::ResourceViewD3D11::ResourceViewD3D11(GpuViewType type, com_ptr<ID3D11View> view)
-    : _type(type)
-    , _view(std::move(view)) {}
-
-up::d3d11::ResourceViewD3D11::~ResourceViewD3D11() = default;
--- source/librender/private/d3d11_backend/d3d11_sampler.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright by Potato Engine contributors. See accompanying License.txt for copyright details.
-
-#include "d3d11_sampler.h"
-
-up::d3d11::SamplerD3D11::SamplerD3D11(com_ptr<ID3D11SamplerState> sampler) : _sampler(std::move(sampler)) {}
-
-up::d3d11::SamplerD3D11::~SamplerD3D11() = default;
