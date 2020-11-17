43a816ad8708483a69279fe9ff9c1e5e047a31c9
Sean Middleditch <sean.middleditch@gmail.com>
2020-11-17 09:35:21 -0800

    Improve unit test

--- source/libreflex/tests/test_schema.cpp
+++ source/libreflex/tests/test_schema.cpp
@@ -8,11 +8,6 @@ TEST_CASE("TestSchema", "[potato][reflex]") {
     using namespace up;
     using namespace up::schema;
 
-    SECTION("type") {
-        TestEnum e{TestEnum::First};
-        TestStruct s{e};
-    }
-
     SECTION("enumToString") {
         CHECK("First"_sv == reflex::enumToString(TestEnum::First));
         CHECK("Third"_sv == reflex::enumToString(TestEnum::Third));
@@ -21,5 +16,13 @@ TEST_CASE("TestSchema", "[potato][reflex]") {
     SECTION("enumToValue") {
         CHECK(TestEnum::First == reflex::enumToValue<TestEnum>("First"_sv));
         CHECK(TestEnum::Third == reflex::enumToValue<TestEnum>("Third"_sv));
+    }
+
+    SECTION("getSchema") {
+        reflex::Schema const& schema = reflex::getSchema<TestStruct>();
+        CHECK(schema.name == "TestStruct"_sv);
+        CHECK(schema.primitive == reflex::SchemaPrimitive::Object);
+        REQUIRE(schema.fields.size() == 1);
+        CHECK(schema.fields[0].name == "test"_sv);
     }
 }
