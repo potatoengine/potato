d79135147905db9a4f696cc9ffb1c9448aba86bc
Sean Middleditch <sean.middleditch@gmail.com>
2020-11-17 19:34:41 -0800

    Add tracy as a dependency

---
+++ external/tracy.cmake
@@ -0,0 +1,10 @@
+include(config.cmake)
+
+FetchContent_Populate(tracy)
+add_library(tracy STATIC)
+target_sources(tracy PRIVATE
+    "${tracy_SOURCE_DIR}/TracyClient.cpp"
+    "${tracy_SOURCE_DIR}/Tracy.hpp"
+)
+target_include_directories(tracy PUBLIC "${tracy_SOURCE_DIR}")
+target_compile_definitions(tracy PUBLIC TRACY_ENABLE=1)
