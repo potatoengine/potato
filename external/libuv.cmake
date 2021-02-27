cb2cc4bff7bd7c60e69e54ea18fe9e159c71ec68
Sean Middleditch <sean.middleditch@gmail.com>
2021-02-27 15:47:09 -0800

    Don't rely on find_package for system libuv

--- external/libuv.cmake
+++ external/libuv.cmake
@@ -1,7 +1,9 @@
 include(config.cmake)
 
-find_package(uv CONFIG QUIET)
-if (NOT TARGET uv)
+find_library(libuv_LIBRARY NAMES uv libuv)
+mark_as_advanced(libuv_LIBRARY)
+
+if (NOT libuv_LIBRARY)
     FetchContent_Populate(libuv)
     add_subdirectory(${libuv_SOURCE_DIR} ${libuv_BINARY_DIR} EXCLUDE_FROM_ALL)
 
@@ -16,4 +18,11 @@ if (NOT TARGET uv)
             /wd4702 # unreachable code
         )
     endif()
+else()
+    find_path(libuv_INCLUDE_DIR NAMES uv.h)
+    mark_as_advanced(libuv_INCLUDE_DIR)
+
+    add_library(uv INTERFACE)
+    target_link_libraries(uv INTERFACE ${libuv_LIBRARY})
+    target_include_directories(uv INTERFACE ${libuv_INCLUDE_DIR})
 endif()
