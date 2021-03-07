9769b288dd40d1421f8d21cd8f8c0eb2d29deaaa
Sean Middleditch <sean.middleditch@gmail.com>
2021-03-06 18:49:13 -0800

    Remove the need to use prepared statements in posql

--- source/libposql/public/potato/posql/posql.h
+++ source/libposql/public/potato/posql/posql.h
@@ -4,8 +4,10 @@
 
 #include "_export.h"
 
+#include "potato/runtime/assertion.h"
 #include "potato/runtime/uuid.h"
 #include "potato/spud/concepts.h"
+#include "potato/spud/rc.h"
 #include "potato/spud/typelist.h"
 #include "potato/spud/zstring_view.h"
 
@@ -20,15 +22,99 @@ namespace up {
     inline namespace posql {
         enum class SqlResult { Ok = 0, Error };
 
+        namespace sqlutil {
+            [[nodiscard]] UP_POSQL_API sqlite3_stmt* compile(sqlite3* db, string_view sql);
+            [[nodiscard]] UP_POSQL_API bool isComplete(sqlite3_stmt* stmt) noexcept;
+            UP_POSQL_API void nextRow(sqlite3_stmt* stmt) noexcept;
+            UP_POSQL_API void reset(sqlite3_stmt* stmt) noexcept;
+            UP_POSQL_API void destroy(sqlite3_stmt* stmt) noexcept;
+
+            [[nodiscard]] UP_POSQL_API int64 fetchColumnI64(sqlite3_stmt* stmt, int index) noexcept;
+            [[nodiscard]] UP_POSQL_API zstring_view fetchColumnString(sqlite3_stmt* stmt, int index) noexcept;
+
+            UP_POSQL_API void bindParam(sqlite3_stmt* stmt, int index, int64 value) noexcept;
+            UP_POSQL_API void bindParam(sqlite3_stmt* stmt, int index, string_view value) noexcept;
+            UP_POSQL_API void bindParam(sqlite3_stmt* stmt, int index, UUID const& value) noexcept;
+            template <enumeration E>
+            void bindParam(sqlite3_stmt* stmt, int index, E value) noexcept {
+                bindParam(stmt, index, to_underlying(value));
+            }
+
+            template <typename T>
+            auto fetchColumn(sqlite3_stmt* stmt, int index) {
+                if constexpr (std::is_same_v<T, UUID>) {
+                    return UUID::fromString(fetchColumnString(stmt, index));
+                }
+                else if constexpr (std::is_constructible_v<T, int64>) {
+                    return fetchColumnI64(stmt, index);
+                }
+                else if constexpr (std::is_enum_v<T>) {
+                    return T(fetchColumnI64(stmt, index));
+                }
+                else if constexpr (std::is_constructible_v<T, zstring_view>) {
+                    return fetchColumnString(stmt, index);
+                }
+            }
+
+            template <int... I, typename... T>
+            std::tuple<T...> fetchColumns(sqlite3_stmt* stmt, std::integer_sequence<int, I...>, typelist<T...>) {
+                return std::tuple<T...>(fetchColumn<T>(stmt, I)...);
+            }
+
+            template <typename... T>
+            std::tuple<T...> fetchColumns(sqlite3_stmt* stmt) {
+                return fetchColumns(stmt, std::make_integer_sequence<int, sizeof...(T)>{}, typelist<T...>{});
+            }
+
+            template <int... I, typename... T>
+            void bindParams(sqlite3_stmt* stmt, std::integer_sequence<int, I...>, T const&... args) noexcept {
+                (((void)bindParam(stmt, I, args)), ...);
+            }
+
+            template <typename... T>
+            void bindParams(sqlite3_stmt* stmt, T const&... args) noexcept {
+                if constexpr (sizeof...(T) != 0) {
+                    bindParams(stmt, std::make_integer_sequence<int, sizeof...(args)>{}, args...);
+                }
+            }
+
+            struct Stmt : shared<Stmt> {
+                explicit Stmt(sqlite3_stmt* s) noexcept : stmt(s) {}
+                virtual ~Stmt() { destroy(stmt); }
+                sqlite3_stmt* stmt = nullptr;
+            };
+
+        } // namespace sqlutil
+
         class Database;
         class Statement;
         class Transaction;
 
         template <typename...>
-        class QueryResult;
-        struct QueryResultSentinel {};
+        class Query;
+        struct QuerySentinel {};
         template <typename...>
-        class QueryResultIterator;
+        class Cursor;
+
+        template <typename... T>
+        class Query {
+        public:
+            using value_type = std::tuple<T...>;
+            using size_type = std::size_t;
+            using iterator = Cursor<T...>;
+            using sentinel = QuerySentinel;
+
+            iterator begin() noexcept { return iterator(std::move(_stmt)); }
+            sentinel end() noexcept { return sentinel{}; }
+
+        private:
+            explicit Query(rc<sqlutil::Stmt> stmt) : _stmt(std::move(stmt)) {}
+
+            rc<sqlutil::Stmt> _stmt;
+
+            friend Statement;
+            friend Database;
+        };
 
         class Database {
         public:
@@ -44,12 +130,44 @@ namespace up {
             [[nodiscard]] bool empty() const noexcept { return _conn == nullptr; }
             [[nodiscard]] explicit operator bool() const noexcept { return _conn != nullptr; }
 
-            [[nodiscard]] UP_POSQL_API Statement prepare(zstring_view sql) noexcept;
-            [[nodiscard]] UP_POSQL_API SqlResult execute(zstring_view sql) noexcept;
+            [[nodiscard]] inline Statement prepare(zstring_view sql);
+
+            template <typename... T>
+            [[nodiscard]] SqlResult execute(string_view sql, T const&... args) {
+                UP_ASSERT(_conn != nullptr);
+                sqlite3_stmt* stmt = compile(sql, args...);
+                SqlResult const result = stmt ? SqlResult::Ok : SqlResult::Error;
+                sqlutil::destroy(stmt);
+                return result;
+            }
+
+            template <typename... R, typename... T>
+            [[nodiscard]] Query<R...> query(string_view sql, T const&... args) {
+                UP_ASSERT(_conn != nullptr);
+                return Query<R...>{new_shared<sqlutil::Stmt>(compile(sql, args...))};
+            }
+
+            template <typename... R, typename... T>
+            [[nodiscard]] auto queryOne(string_view sql, T const&... args) {
+                UP_ASSERT(_conn != nullptr);
+                sqlutil::Stmt stmt(compile(sql, args...));
+                auto const rs = sqlutil::fetchColumns<R...>(stmt.stmt);
+                return rs;
+            }
 
             [[nodiscard]] UP_POSQL_API Transaction begin() noexcept;
 
         private:
+            template <typename... T>
+            sqlite3_stmt* compile(string_view sql, T const&... args) {
+                sqlite3_stmt* stmt = sqlutil::compile(_conn, sql);
+                if (stmt != nullptr) {
+                    sqlutil::bindParams(stmt, args...);
+                    sqlutil::nextRow(stmt);
+                }
+                return stmt;
+            }
+
             sqlite3* _conn = nullptr;
         };
 
@@ -76,156 +194,75 @@ namespace up {
             sqlite3* _conn = nullptr;
         };
 
-        template <typename... T>
-        class QueryResult {
-        public:
-            inline ~QueryResult() noexcept;
-
-            using value_type = std::tuple<T...>;
-            using size_type = std::size_t;
-            using iterator = QueryResultIterator<T...>;
-            using sentinel = QueryResultSentinel;
-
-            iterator begin() noexcept { return iterator(_stmt); }
-            sentinel end() noexcept { return sentinel{}; }
-
-            inline size_type size() const noexcept;
-
-        private:
-            explicit QueryResult(Statement& stmt) : _stmt(stmt) {}
-
-            Statement& _stmt;
-
-            friend Statement;
-        };
-
         class Statement {
         public:
             Statement() noexcept = default;
-            explicit Statement(sqlite3_stmt* stmt) noexcept : _stmt(stmt) {}
-            UP_POSQL_API ~Statement() noexcept;
+            explicit Statement(rc<sqlutil::Stmt> stmt) noexcept : _stmt(std::move(stmt)) {}
 
-            Statement(Statement&& rhs) noexcept : _stmt(rhs._stmt) { rhs._stmt = nullptr; }
+            Statement(Statement&& rhs) noexcept : _stmt(std::move(rhs._stmt)) { rhs._stmt = nullptr; }
             UP_POSQL_API Statement& operator=(Statement&& rhs) noexcept;
 
             [[nodiscard]] bool empty() const noexcept { return _stmt == nullptr; }
             [[nodiscard]] explicit operator bool() const noexcept { return _stmt != nullptr; }
 
-            [[nodiscard]] UP_POSQL_API SqlResult execute() noexcept {
-                _begin();
-                return _execute();
-            }
-
             template <typename... T>
             [[nodiscard]] SqlResult execute(T const&... args) noexcept {
-                _begin();
-                _bind(std::make_integer_sequence<int, sizeof...(args)>{}, args...);
-                return _execute();
+                sqlutil::reset(_stmt->stmt);
+                sqlutil::bindParams(_stmt->stmt, args...);
+                sqlutil::nextRow(_stmt->stmt);
+                return SqlResult::Ok;
             }
 
             template <typename... R, typename... T>
-            [[nodiscard]] QueryResult<R...> query(T const&... args) noexcept {
-                _begin();
-                if constexpr (sizeof...(T) != 0) {
-                    _bind(std::make_integer_sequence<int, sizeof...(args)>{}, args...);
-                }
-                _query();
-                return QueryResult<R...>{*this};
+            [[nodiscard]] Query<R...> query(T const&... args) noexcept {
+                sqlutil::reset(_stmt->stmt);
+                sqlutil::bindParams(_stmt->stmt, args...);
+                sqlutil::nextRow(_stmt->stmt);
+                return Query<R...>{_stmt};
             }
 
             template <typename... R, typename... T>
             [[nodiscard]] auto queryOne(T const&... args) noexcept {
-                _begin();
-                if constexpr (sizeof...(T) != 0) {
-                    _bind(std::make_integer_sequence<int, sizeof...(args)>{}, args...);
-                }
-                _query();
-                auto result = _columns<R...>();
-                _finalize();
+                sqlutil::reset(_stmt->stmt);
+                sqlutil::bindParams(_stmt->stmt, args...);
+                sqlutil::nextRow(_stmt->stmt);
+                auto result = sqlutil::fetchColumns<R...>(_stmt->stmt);
+                sqlutil::reset(_stmt->stmt);
                 return result;
             }
 
         private:
-            UP_POSQL_API void _begin() noexcept;
-            UP_POSQL_API SqlResult _execute() noexcept;
-            UP_POSQL_API void _query() noexcept;
-            UP_POSQL_API bool _done() noexcept;
-            UP_POSQL_API void _next() noexcept;
-            UP_POSQL_API std::size_t _rows() noexcept;
-            UP_POSQL_API void _finalize() noexcept;
-
-            template <int... I, typename... T>
-            void _bind(std::integer_sequence<int, I...>, T const&... args) noexcept {
-                (((void)_bind(I, args)), ...);
-            }
-
-            UP_POSQL_API void _bind(int index, int64 value) noexcept;
-            UP_POSQL_API void _bind(int index, string_view value) noexcept;
-            UP_POSQL_API void _bind(int index, UUID const& value) noexcept;
-            template <enumeration E>
-            void _bind(int index, E value) noexcept {
-                _bind(index, to_underlying(value));
-            }
-
-            template <typename... T>
-            std::tuple<T...> _columns() {
-                return _columns(std::make_integer_sequence<int, sizeof...(T)>{}, typelist<T...>{});
-            }
-
-            template <int... I, typename... T>
-            std::tuple<T...> _columns(std::integer_sequence<int, I...>, typelist<T...>) {
-                return std::tuple<T...>(_column<T>(I)...);
-            }
-
-            template <typename T>
-            auto _column(int index) {
-                if constexpr (std::is_same_v<T, UUID>) {
-                    return UUID::fromString(_column_string(index));
-                }
-                else if constexpr (std::is_constructible_v<T, int64>) {
-                    return _column_int64(index);
-                }
-                else if constexpr (std::is_enum_v<T>) {
-                    return T(_column_int64(index));
-                }
-                else if constexpr (std::is_constructible_v<T, zstring_view>) {
-                    return _column_string(index);
-                }
-            }
-
-            [[nodiscard]] UP_POSQL_API int64 _column_int64(int index) noexcept;
-            [[nodiscard]] UP_POSQL_API zstring_view _column_string(int index) noexcept;
-
             template <typename...>
-            friend class QueryResult;
+            friend class Query;
             template <typename...>
-            friend class QueryResultIterator;
+            friend class Cursor;
 
-            sqlite3_stmt* _stmt = nullptr;
+            rc<sqlutil::Stmt> _stmt;
         };
 
         template <typename... T>
-        class QueryResultIterator {
+        class Cursor {
         public:
-            [[nodiscard]] bool operator==(QueryResultSentinel) noexcept { return _stmt._done(); }
-            QueryResultIterator& operator++() noexcept {
-                _stmt._next();
+            [[nodiscard]] bool operator==(QuerySentinel) noexcept {
+                return _stmt == nullptr || sqlutil::isComplete(_stmt->stmt);
+            }
+            Cursor& operator++() noexcept {
+                sqlutil::nextRow(_stmt->stmt);
                 return *this;
             }
-            [[nodiscard]] inline std::tuple<T...> operator*() { return _stmt._columns<T...>(); }
+            [[nodiscard]] inline std::tuple<T...> operator*() { return sqlutil::fetchColumns<T...>(_stmt->stmt); }
 
         private:
-            explicit QueryResultIterator(Statement& stmt) : _stmt(stmt) {}
+            explicit Cursor(rc<sqlutil::Stmt> stmt) : _stmt(std::move(stmt)) {}
 
-            Statement& _stmt;
+            rc<sqlutil::Stmt> _stmt;
 
             template <typename...>
-            friend class QueryResult;
+            friend class Query;
         };
 
-        template <typename... T>
-        QueryResult<T...>::~QueryResult() noexcept {
-            _stmt._finalize();
+        Statement Database::prepare(zstring_view sql) {
+            return Statement(new_shared<sqlutil::Stmt>(sqlutil::compile(_conn, sql)));
         }
     }; // namespace posql
 
