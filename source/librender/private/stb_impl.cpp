3bc280b2a4eb5b71dc4c150f3104a9dc898fbc67
Sean Middleditch <sean.middleditch@gmail.com>
2021-03-09 08:59:50 -0800

    Silence warnings from updated stb

--- source/librender/private/stb_impl.cpp
+++ source/librender/private/stb_impl.cpp
@@ -2,4 +2,10 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+
 #include <stb_image.h>
+
+#pragma GCC diagnostic pop
